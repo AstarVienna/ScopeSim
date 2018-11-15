80a9af372c7b64ba1bb00e6854df320640dffee6
astronomyk <kdleschinski@gmail.com>
2018-11-15 11:14:22 +0100

    Refactoring import statements

--- telescopy/__init__.py
+++ telescopy/__init__.py
@@ -7,12 +7,14 @@ from . import detector
 from . import optics
 from . import source
 from . import simulation
+from . import utils
 
 from .version import __version__
+from .default_keywords import *
 
 # turn off warnings - interesting for development, but not for runtime
 #warnings.simplefilter('ignore', UserWarning)   # user should see UserWarnings
-warnings.simplefilter('ignore', FutureWarning)
-warnings.simplefilter('ignore', RuntimeWarning)  # warnings for the developer
-warnings.simplefilter('ignore', category=AstropyWarning)
+# warnings.simplefilter('ignore', FutureWarning)
+# warnings.simplefilter('ignore', RuntimeWarning)  # warnings for the developer
+# warnings.simplefilter('ignore', category=AstropyWarning)
 
--- telescopy/default_keywords.py
+++ telescopy/default_keywords.py
@@ -8,14 +8,14 @@
 ## It does NOT contain information regarding the instrument or telescope 
 ## configuration
 
-import os
-import inspect
+from os.path import dirname
+from inspect import getfile, currentframe
 
 # Package directory
-PKG_DIR = os.path.dirname(inspect.getfile(inspect.currentframe()))
+PKG_DIR = dirname(getfile(currentframe()))
 
 # Instrument package paths
-INST_PKG_LOCAL_PATH     = "inst_pkgs/"
+INST_PKG_LOCAL_PATH     = "INST_PKGS/"
 INST_PKG_LOCAL_DB_NAME  = "LocalInstPkgDB.dat"
 INST_PKG_SERVER_PATH    = "https://www.univie.ac.at/simcado/InstPkgSvr/"
 INST_PKG_SERVER_DB_NAME = "InstPkgDB.dat"
