31fbae8206c6ce1a5aac1b544332cc11a45d8f05
Kieran <kdleschinski@gmail.com>
2019-08-06 07:40:55 +0200

    test to see if I can make Travis-only tests

--- scopesim/tests/tests_effects/test_SkycalcTERCurve.py
+++ scopesim/tests/tests_effects/test_SkycalcTERCurve.py
@@ -4,6 +4,8 @@ from synphot import SpectralElement, SourceSpectrum
 from scopesim.effects import SkycalcTERCurve
 from scopesim import rc
 
+TRAVIS = True if "TRAVIS" in os.environ else False
+
 
 def teardown_module():
     if os.path.exists("skycalc_temp.fits"):
@@ -12,15 +14,19 @@ def teardown_module():
 
 class TestInit:
     def test_initialises_with_nothing(self):
-        assert isinstance(SkycalcTERCurve(), SkycalcTERCurve)
+        if TRAVIS:
+            assert isinstance(SkycalcTERCurve(), SkycalcTERCurve)
+            assert False
 
     def test_initialises_with_some_kwargs(self):
-        sky_ter = SkycalcTERCurve(pwv=1.0, observatory="paranal")
-        assert str(sky_ter.surface.meta["wavelength_unit"]) == "um"
+        if TRAVIS:
+            sky_ter = SkycalcTERCurve(pwv=1.0, observatory="paranal")
+            assert str(sky_ter.surface.meta["wavelength_unit"]) == "um"
 
     def test_initialises_with_bang_strings(self):
-        rc.__currsys__["!OBS.pwv"] = 20.0
-        sky_ter = SkycalcTERCurve(pwv="!OBS.pwv")
-        assert sky_ter.skycalc_conn.values["pwv"] == 20.0
-        assert isinstance(sky_ter.surface.transmission, SpectralElement)
-        assert isinstance(sky_ter.surface.emission, SourceSpectrum)
+        if TRAVIS:
+            rc.__currsys__["!OBS.pwv"] = 20.0
+            sky_ter = SkycalcTERCurve(pwv="!OBS.pwv")
+            assert sky_ter.skycalc_conn.values["pwv"] == 20.0
+            assert isinstance(sky_ter.surface.transmission, SpectralElement)
+            assert isinstance(sky_ter.surface.emission, SourceSpectrum)
