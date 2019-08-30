229965a3d7d8936739feec2431ebafcbf471aba7
Kieran <kdleschinski@gmail.com>
2019-08-30 15:43:19 +0200

    finished, but look into splitting up MICADO imagePlane for 2D effects. Otherwise memory error might occur

--- scopesim/tests/tests_integrations/test_micado/test_full_package_micado.py
+++ scopesim/tests/tests_integrations/test_micado/test_full_package_micado.py
@@ -28,7 +28,7 @@ PLOTS = False
 
 def setup_module():
     rc_local_path = rc.__config__["!SIM.file.local_packages_path"]
-    if not os.path.exists(rc_local_path):
+    if not os.path.exists(rc_local_path) or CLEAN_UP:
         os.mkdir(rc_local_path)
         rc.__config__["!SIM.file.local_packages_path"] = os.path.abspath(
             rc_local_path)
@@ -65,7 +65,10 @@ class TestLoadUserCommands:
 
     def test_user_commands_loads_mode_files(self):
         cmd = scopesim.UserCommands(use_instrument="MICADO")
-        assert "MICADO_IMG_LR" in [yd["name"] for yd in cmd.yaml_dicts]
+        yaml_names = [yd["name"] for yd in cmd.yaml_dicts]
+        print(yaml_names)
+
+        assert "MICADO_IMG_LR" in yaml_names
 
     def test_user_commands_can_change_modes(self):
         cmd = scopesim.UserCommands(use_instrument="MICADO")
@@ -82,17 +85,14 @@ class TestLoadUserCommands:
 
 class TestMakeOpticalTrain:
     def test_works_seamlessly_for_micado_package(self, capsys):
-        from time import time
-        start = time()
 
+        src = scopesim.source.source_templates.star_field(10000, 10, 25, 60)
+        src = scopesim.source.source_templates.empty_sky()
         cmd = scopesim.UserCommands(use_instrument="MICADO")
         opt = scopesim.OpticalTrain(cmd)
         assert isinstance(opt, scopesim.OpticalTrain)
 
-        print(time()-start)
-
-        tc = opt.optics_manager.surfaces_table.throughput
+        # ..todo:: add source object here!
+        # opt.observe(src)
         hdu = opt.readout()
         assert isinstance(hdu, fits.HDUList)
-
-        print(time()-start)
