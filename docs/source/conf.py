542cd3d10ac309ae787bc89a8e708723e1d98d1d
Kieran <kdleschinski@gmail.com>
2020-07-06 12:33:54 +0200

    made setup up for 5-liners

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -217,11 +217,3 @@ intersphinx_mapping = {'python': ('http://docs.python.org/', None),
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
-
-
-import os, scopesim
-
-if not os.path.exists("./temp/"): os.mkdir("./temp/")
-scopesim.rc.__config__["!SIM.file.local_packages_path"] = "./temp/"
-pkg_names = ["locations/Paranal", "telescopes/VLT", "instruments/HAWKI"]
-scopesim.server.download_package(pkg_names)
