acca7e0f99778e0db3b727ede3fe50e728a827ff
Kieran <kdleschinski@gmail.com>
2021-04-08 14:13:56 +0200

    minor change to tests

--- scopesim/tests/tests_source/test_cube_source.py
+++ scopesim/tests/tests_source/test_cube_source.py
@@ -15,65 +15,64 @@ MOCK_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__),
                                         "../mocks/files/"))
 sim.rc.__search_path__.insert(0, MOCK_DIR)
 
-PLOTS = True
+PLOTS = False
 
 
-def test_source_cube_angstrom():
-    """
-    Testing basics
-    """
+class TestSourceInitWithCube:
+    def test_accepts_raw_image_hdu(self):
+        """
+        Testing basics
+        """
 
-    cube = _make_dummy_cube(scale=0.2 * u.arcsec,
-                            wave_unit=u.AA, ref_wave=1000*u.AA,
-                            wave_step=1*u.AA, wave_type="WAVE",
-                            bunit="erg / (s angstrom cm2 arcsec2)")
+        cube = _make_dummy_cube(scale=0.2 * u.arcsec,
+                                wave_unit=u.AA, ref_wave=1000*u.AA,
+                                wave_step=1*u.AA, wave_type="WAVE",
+                                bunit="erg / (s angstrom cm2 arcsec2)")
 
-    src_cube = Source(cube=cube)
+        src_cube = Source(cube=cube)
 
-    if PLOTS:
-        src_cube.plot()
-        plt.show()
+        if PLOTS:
+            src_cube.plot()
+            plt.show()
 
-    assert isinstance(src_cube.fields[0], fits.ImageHDU)
+        assert isinstance(src_cube.fields[0], fits.ImageHDU)
 
+    def test_accepts_multiple_cubes_as_hdu_list(self):
+        """
+        Testing basics
+        """
 
-def test_source_hdu_list():
-    """
-    Testing basics
-    """
+        cube = _make_dummy_cube(scale=0.2 * u.arcsec,
+                                wave_unit=u.AA, ref_wave=1000*u.AA, wave_step=1*u.AA, wave_type="WAVE",
+                                bunit="erg / (s angstrom cm2 arcsec2)")
 
-    cube = _make_dummy_cube(scale=0.2 * u.arcsec,
-                            wave_unit=u.AA, ref_wave=1000*u.AA, wave_step=1*u.AA, wave_type="WAVE",
-                            bunit="erg / (s angstrom cm2 arcsec2)")
+        cube_list = fits.HDUList(hdus=[cube, cube])
 
-    cube_list = fits.HDUList(hdus=[cube, cube])
+        src_cube = Source(cube=cube_list, ext=1)
 
-    src_cube = Source(cube=cube_list, ext=1)
+        if PLOTS:
+            src_cube.plot()
+            plt.show()
 
-    if PLOTS:
-        src_cube.plot()
-        plt.show()
+        assert isinstance(src_cube.fields[0], fits.ImageHDU)
 
-    assert isinstance(src_cube.fields[0], fits.PrimaryHDU)
+    def test_file_to_cube(self):
+        """
+        Testing basics
+        """
 
+        cube = _make_dummy_cube(scale=0.2 * u.arcsec,
+                                wave_unit=u.AA, ref_wave=1000*u.AA, wave_step=1*u.AA, wave_type="WAVE",
+                                bunit="erg / (s angstrom cm2 arcsec2)")
 
-def test_source_from_file():
-    """
-    Testing basics
-    """
+        filename = os.path.join(MOCK_DIR, "cube.fits")
+        cube.writeto(filename, overwrite=True)
 
-    cube = _make_dummy_cube(scale=0.2 * u.arcsec,
-                            wave_unit=u.AA, ref_wave=1000*u.AA, wave_step=1*u.AA, wave_type="WAVE",
-                            bunit="erg / (s angstrom cm2 arcsec2)")
+        src_cube = Source(cube=filename, ext=1)
 
-    filename = os.path.join(MOCK_DIR, "cube.fits")
-    cube.writeto(filename)
+        if PLOTS:
+            src_cube.plot()
+            plt.show()
 
-    src_cube = Source(cube=filename)
-
-    if PLOTS:
-        src_cube.plot()
-        plt.show()
-
-    assert isinstance(src_cube.fields[0], fits.PrimaryHDU)
+        assert isinstance(src_cube.fields[0], fits.ImageHDU)
 
