95de027fbd8ab217b46b10a8ee290773d866ab3e
Kieran <kdleschinski@gmail.com>
2021-07-12 16:42:44 +0200

    added roataional blurring to PSF objects. Minor changes to chop_nod effect. New version number

--- scopesim/tests/__init__.py
+++ scopesim/tests/__init__.py
@@ -1,7 +1,7 @@
 import os
 from scopesim import rc
 
-rc.__config__["!SIM.tests.run_integration_tests"] = False
+rc.__config__["!SIM.tests.run_integration_tests"] = True
 rc.__config__["!SIM.tests.run_skycalc_ter_tests"] = True
 rc.__config__["!SIM.file.use_cached_downloads"] = False
 rc.__config__["!SIM.reports.ip_tracking"] = False
