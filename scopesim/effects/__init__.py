221440b235d0daf0b121178af3b0ccb9146dff65
oczoske <ollicz68@yahoo.com>
2021-10-19 11:07:54 +0200

    remove pupil_mask

--- scopesim/effects/__init__.py
+++ scopesim/effects/__init__.py
@@ -9,7 +9,6 @@ from .spectral_trace_list import *
 from .surface_list import *
 from .ter_curves import *
 from . import ter_curves_utils
-from .pupil_mask import *
 
 from .detector_list import *
 from .electronic import *
