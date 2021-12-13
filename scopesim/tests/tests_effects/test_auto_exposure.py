09ca99d87e50e312bbdf08d9f33ddb6c408c9b03
oczoske <ollicz68@yahoo.com>
2021-12-13 13:45:46 +0100

    tweak AutoExposure, modif to DarkCurrent

--- scopesim/tests/tests_effects/test_auto_exposure.py
+++ scopesim/tests/tests_effects/test_auto_exposure.py
@@ -31,22 +31,24 @@ class TestAutoExposure:
     def test_initialises_correctly(self):
         autoexposure = AutoExposure(fill_frac=0.75,
                                     full_well=1.e5,
-                                    exptime=None,
                                     mindit=0.011)
         assert isinstance(autoexposure, AutoExposure)
 
     def test_returns_imageplane(self, autoexposure, imageplane):
-        rc.__currsys__ = UserCommands(properties={"!OBS.dit": 60,
-                                                  "!OBS.ndit": 2})
+        rc.__currsys__ = UserCommands(properties={"!OBS.exptime": 1.,
+                                                  "!OBS.dit": None,
+                                                  "!OBS.ndit": None})
         outimpl = autoexposure.apply_to(imageplane)
         assert isinstance(outimpl, ImagePlane)
 
     def test_produces_correct_values(self, autoexposure, imageplane):
         in_dit = 50
-        in_ndit = 2
+        in_ndit = 3
         exptime = in_dit * in_ndit
-        rc.__currsys__ = UserCommands(properties={"!OBS.dit": in_dit,
-                                                  "!OBS.ndit": in_ndit})
+        # TODO: Change AutoExposure to read exptime like dit and ndit
+        rc.__currsys__ = UserCommands(properties={"!OBS.exptime": exptime,
+                                                  "!OBS.dit": None,
+                                                  "!OBS.ndit": None})
 
         # imageplane has 1e5 e/s, full_well is 1e5 e. To fill to 75% need:
         ref_dit = 0.75
@@ -55,59 +57,69 @@ class TestAutoExposure:
 
         out_dit = from_currsys("!OBS.dit")
         out_ndit = from_currsys("!OBS.ndit")
-        assert out_dit == ref_dit
-        assert out_dit * out_ndit >= exptime
-        assert out_dit * (out_ndit - 1) < exptime
+        assert out_dit == pytest.approx(ref_dit)
+        assert out_dit * out_ndit == pytest.approx(exptime)
 
     def test_detects_saturation(self, imageplane):
         mindit = 0.011
+        rc.__currsys__ = UserCommands(properties={"!OBS.exptime": 100.,
+                                                  "!OBS.dit": None,
+                                                  "!OBS.ndit": None})
+
         autoexposure = AutoExposure(fill_frac=0.75,
                                     full_well=10.,
-                                    exptime=None,
                                     mindit=mindit)
         autoexposure.apply_to(imageplane)
 
         out_dit = from_currsys("!OBS.dit")
         assert out_dit == mindit
 
-    def test_exptime_overrides_dit_ndit(self, imageplane):
-        rc.__currsys__ = UserCommands(properties={"!OBS.dit": 1,
-                                                  "!OBS.ndit": 1})
-        exptime = 100.
-        autoexposure = AutoExposure(fill_frac=0.75,
-                                    full_well=1e5,
-                                    exptime=exptime,
-                                    mindit=0.011)
-        autoexposure.apply_to(imageplane)
-
-        out_dit = from_currsys("!OBS.dit")
-        out_ndit = from_currsys("!OBS.ndit")
-
-        assert out_dit * out_ndit >=exptime
-        assert out_dit * (out_ndit - 1) < exptime
-
     def test_fill_frac_acts_correctly(self, imageplane):
         fill_1 = 1.
         fill_2 = 0.5
         autoexp_1 = AutoExposure(fill_frac = fill_1,
                                  full_well = 1e5,
-                                 exptime=None,
                                  mindit=0.011)
-        rc.__currsys__ = UserCommands(properties={"!OBS.dit": 1,
-                                                  "!OBS.ndit": 1})
+        rc.__currsys__ = UserCommands(properties={"!OBS.exptime": 1,
+                                                  "!OBS.dit": None,
+                                                  "!OBS.ndit": None})
         autoexp_1.apply_to(imageplane)
         out_dit_1 = from_currsys("!OBS.dit")
         out_ndit_1 = from_currsys("!OBS.ndit")
 
         autoexp_2 = AutoExposure(fill_frac = fill_2,
                                  full_well = 1e5,
-                                 exptime=None,
                                  mindit=0.011)
-        rc.__currsys__ = UserCommands(properties={"!OBS.dit": 1,
-                                                  "!OBS.ndit": 1})
+        rc.__currsys__ = UserCommands(properties={"!OBS.exptime": 1,
+                                                  "!OBS.dit": None,
+                                                  "!OBS.ndit": None})
         autoexp_2.apply_to(imageplane)
         out_dit_2 = from_currsys("!OBS.dit")
         out_ndit_2 = from_currsys("!OBS.ndit")
 
         assert out_dit_1 == fill_1 / fill_2 * out_dit_2
         assert out_ndit_1 == fill_2 / fill_1 * out_ndit_2
+
+    def test_exptime_specified_by_dit_ndit(self, autoexposure, imageplane):
+        """
+        Test that exptime can be given by `!OBS.dit` and `!OBS.ndit`
+        instead of `!OBS.exptime`.
+        """
+        # 1. use exptime
+        rc.__currsys__ = UserCommands(properties={"!OBS.exptime": 10,
+                                                  "!OBS.dit": None,
+                                                  "!OBS.ndit": None})
+        autoexposure.apply_to(imageplane)
+        dit_1 = from_currsys("!OBS.dit")
+        ndit_1 = from_currsys("!OBS.ndit")
+
+        # 2. use dit and ndit
+        rc.__currsys__ = UserCommands(properties={"!OBS.exptime": None,
+                                                  "!OBS.dit": 5,
+                                                  "!OBS.ndit": 2})
+        autoexposure.apply_to(imageplane)
+        dit_2 = from_currsys("!OBS.dit")
+        ndit_2 = from_currsys("!OBS.ndit")
+
+        assert dit_1 == dit_1
+        assert ndit_1 == ndit_2
