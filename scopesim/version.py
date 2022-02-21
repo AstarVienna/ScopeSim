23c16e6ceeb12ab22102709ee20a101912f1358e
Kieran <kdleschinski@gmail.com>
2022-02-21 10:33:46 +0100

    added v0.2.0 change log description to version.py

--- scopesim/version.py
+++ scopesim/version.py
@@ -1,2 +1,15 @@
 version = '0.2.0'
 date    = '2022-02-17 08:30:00 GMT'
+yaml_descriptions = """
+- version : 0.2.0
+  date : 2022-02-17
+  comment : Version released for the METIS science team
+  changes : 
+  - release of new spectroscopy effect SpectalTraceList
+  - moved individual spectral trace code to utils file
+  - rewritten FovManager class 
+  - added make_cube, make_image, make_spectrum to FieldOfView class
+  - removed fov_grid from Effects
+  - added new detector array z_order section (900-class effects)  
+
+"""
