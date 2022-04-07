6bd7c7e5a9fd3c9637f51c38e1723b4f72a605e0
Kieran <kdleschinski@gmail.com>
2022-04-07 12:08:00 +0200

    simple img and spec tests passing

--- scopesim/tests/mocks/basic_instrument/code/make_spectral_trace.py
+++ scopesim/tests/mocks/basic_instrument/code/make_spectral_trace.py
@@ -4,7 +4,7 @@
 - H: 1.3, 1.8      dwave = 0.5  --> R~2000
 - K: 1.75, 2.5     dwave = 0.75 --> R~1300
 
-The detector is 1024 * 0.2 = 204.8" [+/-102.4"] and 10.24mm [+/-5.21mm]
+The detector is 1024 * 0.2 = 204.8" [+/-102.4"] and 10.24mm [+/-5.12mm]
 Slit is 60", or 300 pixel, so trace x-borders should be:
 - J: (-95, -35) ["], --> /20 "/mm --> (-4.75, -1.75) [mm] centred at -3.25
 - H: (-30,  30) ["], --> /20 "/mm --> (-1.5,   1.5)  [mm] centred at 0
@@ -38,12 +38,15 @@ dss = np.array([-30., 10., 0., 10., 30.])
 
 hdus = []
 for wave_min, wave_max, s_cen, name in zip(wave_mins, wave_maxs, s_cens, names):
-    n = 11
-    waves = np.geomspace(wave_min, wave_max, 11)
+    n = 101
+
+    waves = np.geomspace(wave_min, wave_max, n)
     waves = np.array([[w]*5 for w in waves]).flatten()
+
+    ys = np.array([[y]*5 for y in np.linspace(-5, 5, n)]).flatten()
+
     ss = np.array(list(dss) * n)
     xs = (ss + s_cen) / 20.
-    ys = np.array([[y]*5 for y in np.linspace(-5, 5, n)]).flatten()
 
     tbl = Table(names=["wavelength", "s", "x", "y"],
                 data=[waves, ss, xs, ys],
@@ -65,4 +68,4 @@ pri_hdr.header["ECAT"] = 1         # where to find the catalogue table
 pri_hdr.header["EDATA"] = 2        # where the data tables start
 
 hdul = fits.HDUList([pri_hdr, cat_hdu] + hdus)
-hdul.writeto("INS_lss_traces.fits", overwrite=True)
+hdul.writeto("../INS_lss_traces.fits", overwrite=True)
