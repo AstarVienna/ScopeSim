025d1d77c94b5360ed5dd43f36559f647d7c9f3b
Kieran <kdleschinski@gmail.com>
2022-04-13 11:24:52 +0200

    refactor and test of SkyCalcTERCurve for overriding with local file

--- scopesim/tests/tests_effects/test_SkycalcTERCurve.py
+++ scopesim/tests/tests_effects/test_SkycalcTERCurve.py
@@ -9,6 +9,10 @@ from scopesim.utils import from_currsys
 if rc.__config__["!SIM.tests.run_skycalc_ter_tests"] is False:
     pytestmark = pytest.mark.skip("Ignoring SkyCalc integration tests")
 
+FILES_PATH = os.path.join(os.path.dirname(__file__), "../MOCKS/files/")
+if FILES_PATH not in rc.__search_path__:
+    rc.__search_path__ += [FILES_PATH]
+
 
 def setup_module():
     rc_local_path = rc.__config__["!SIM.file.local_packages_path"]
@@ -27,6 +31,10 @@ class TestInit:
     def test_initialises_with_nothing(self):
         assert isinstance(SkycalcTERCurve(), SkycalcTERCurve)
 
+    def test_initialises_with_include_false_has_no_table(self):
+        sky_ter = SkycalcTERCurve(include=False)
+        assert sky_ter.skycalc_table is None
+
     def test_initialises_with_some_kwargs(self):
         sky_ter = SkycalcTERCurve(pwv=1.0, observatory="paranal")
         assert str(sky_ter.surface.meta["wavelength_unit"]) == "um"
@@ -41,3 +49,7 @@ class TestInit:
     def test_initialises_with_non_skycalc_keys(self):
         sky_ter = SkycalcTERCurve(name="bogus")
         assert "name" not in sky_ter.skycalc_conn.values
+
+    def test_initialise_with_local_skycalc_file(self):
+        sky_ter = SkycalcTERCurve(use_local_skycalc_file="skycalc_override.fits")
+        assert sky_ter.skycalc_table is not None
