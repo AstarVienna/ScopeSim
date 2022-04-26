ae7e5f5949c8a9a09e8182102b5cce4502c5abb8
Kieran <kdleschinski@gmail.com>
2022-04-26 16:13:26 +0200

    green

--- scopesim/tests/tests_source/test_source_templates.py
+++ scopesim/tests/tests_source/test_source_templates.py
@@ -5,6 +5,7 @@ from _pytest.python_api import approx
 from astropy import units as u
 from astropy.table import Table
 
+from scopesim import load_example_optical_train
 from scopesim.source import source_templates as src_ts
 from scopesim.source.source import Source
 
@@ -51,8 +52,8 @@ def test_all_zero_spectra_line_up():
 
 class TestUniformIllumination:
     def test_makes_source_and_runs_through_basic_instrument(self):
-        from scopesim import load_example_optical_train
         opt = load_example_optical_train()
+
         src = src_ts.uniform_illumination(xs=[-50, 50], ys=[20, 30],
                                           pixel_scale=1, flux=1*u.mag)
         opt.observe(src)
