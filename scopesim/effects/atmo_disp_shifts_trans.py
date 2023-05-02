330322a08888f38fdd15fb98068e97069c2d2990
Jay Stephan <Jay.Stephan@stfc.ac.uk>
2023-05-02 13:38:16 +0100

    Second commit of MOSAIC AD Effects - code altered to fit SCOPESIM purpose Now outputs WLs and trans for individual fibre

---
+++ scopesim/effects/atmo_disp_shifts_trans.py
@@ -0,0 +1,268 @@
+import numpy as np
+from astropy import units as u
+
+from astropy.coordinates import Angle
+
+import atmo_disp_shifts_trans_utils as disp_utils
+
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+plt.style.use('bmh')
+
+class AD_simulation:
+    """
+    Class to quantify atmospheric dispersion effects on a MOSAIC integration.
+    See .run for inputs and outputs
+    
+    Author: Jay Stephan
+    
+    Bugs/Issues:
+    HR is not currently implemented fully
+    """
+    def __init__(self,**kwargs):
+        params = {'telescope_diameter':39, #m, diameter of telescope (ELT)
+                  'wavefront_outerscale':46, #m, wavefront out scale for FWHM change with airmass/wavelength
+                  'median_seeing':.68, #arcsec, median seeing at Paranal
+                  'median_seeing_wl':.5, #um, wavelength median seeing corresponds to
+
+                  'latitude':-24.6272, #deg, MUST be negative (southern hemisphere) for now
+                  'temperature':10+273.15, #K, temperature at Paranal
+                  'humidity':14.5/100, #fraction, relative humidity at Paranal
+                  'pressure':750, #mbar, pressure at Paranal
+                  
+                  'LR_VIS_major_axis':.702, #arcsec, major axis of observing modes apertures
+                  'LR_NIR_major_axis':.57, 
+                  'HR_VIS_major_axis':.700, 
+                  'HR_NIR_major_axis':.57, #This needs to be confirmed, but should be correct
+                           
+                  'LR_VIS_B':[.390,.458], #um, MOSAIC bands
+                  'LR_VIS_G':[.450,.591], 
+                  'LR_VIS_R':[.586,.770], 
+                  'LR_VIS_All':[.390,.770], 
+                  'LR_NIR_IY':[.770,1.063], 
+                  'LR_NIR_J':[1.01,1.395], 
+                  'LR_NIR_H':[1.420,1.857], 
+                  'LR_NIR_All':[.770,1.857], 
+                  'HR_VIS_G':[0,0], #HR values need to be confirmed, especially VIS
+                  'HR_VIS_R':[0,0],
+                  'HR_VIS_All':[0,0],
+                  'HR_NIR_J':[0,0],
+                  'HR_NIR_H':[0,0],
+                  'HR_NIR_All':[0,0],
+
+                  'sim_scale':.005, #arcsec/pixel, scale to carry out the simulation - smaller is slower, more accurate
+                  'sim_HA_samples':21, #number of instantaneous snapshots to average over for the integration                 
+                  'relative_plate_PA_angle':0, #deg, relative angle of the plate/apertures and PA=0. For PA=0 along semi major axis, set to 90 deg
+                  }
+        for kwarg in kwargs.items():
+            params[kwarg[0]]=kwarg[1]
+        self.config=params
+        self.input={}
+        self.output={}
+        
+    def load_MOSAIC_band(self,band,sampling):
+        """
+        Generates wavelengths for the simulation from the chosen band, 
+        and acquires relevant diameter for the band (changes between LR/HR, and VIS/NIR)
+        """
+        self.input['sampling']=sampling
+        self.input['band']=band
+        
+        wave_min,wave_max= self.config[band][0],self.config[band][1]
+
+        self.output['wavelengths'] = np.arange(wave_min,wave_max,sampling)   
+        self.output['major_axis']=self.config[band[0:6]+"_major_axis"]
+        
+    def load_HA(self,HA_start,HA_end,declination):
+        """
+        
+        """
+        HA_range=np.linspace(HA_start,HA_end,self.config['sim_HA_samples'])
+        self.input['HA_range']=HA_range
+        self.input['targ_dec']=declination
+
+        #latitude needs to be negative for now
+        lat = self.config['latitude'] * np.pi/180
+        dec = declination*np.pi/180
+        
+        #Need to check if the target is below the horizon for the given list of HA, and if so remove it.
+        LHA_below_horizon=np.rad2deg(np.arccos(-np.tan(lat)*np.tan(dec)))/15 
+        if str(LHA_below_horizon) != 'nan': 
+            for val in HA_range.copy(): 
+                if abs(val) > abs(LHA_below_horizon):
+                    print("At HA %2.2fh, target goes below horizon - removing this from HA range" % (val))
+                    HA_range.remove(val)        
+        if dec > np.pi/2 + lat: #If the target has a too high declination, it will never be seen at Cerro Paranal
+            print("Target always below Horizon")
+            return
+
+        airmasses=1/(np.sin(lat)*np.sin(dec)+np.cos(lat)*np.cos(dec)*np.cos(Angle(HA_range*u.hour).rad))
+        self.output['airmasses']=np.array(airmasses)
+        
+        para_angles=disp_utils.parallatic_angle(np.array(HA_range),dec,lat)
+        self.output['raw_para_angles']=np.array(para_angles) #actual PAs
+        
+    def calculate_shifts(self, guide_waveref, aperture_waveref):
+        """
+        
+        """  
+        self.input['guide_waveref']=guide_waveref
+        self.input['aperture_waveref']=aperture_waveref
+
+        airmasses=self.output['airmasses']
+        wavelengths=self.output['wavelengths']
+
+        #centring refers to the index of the hour angles at which we centre the aperture/guiding on a wavelength
+        centring_index=int((len(airmasses)-1)/2)
+
+        centre_shift=disp_utils.diff_shift(aperture_waveref,airmasses[centring_index],guide_waveref,self.config) #shift of the original aperture centre wavelength from guide wavelength
+        centring_q=self.output['raw_para_angles'][centring_index]
+
+        raw_para_angles=self.output['raw_para_angles']
+        para_angles=self.output['raw_para_angles'].copy()
+        for i in range(0,len(para_angles)): #change in PAs from centring index
+            para_angles[i]=para_angles[i]-self.output['raw_para_angles'][centring_index]
+
+        shifts_para=[]
+        phi=np.deg2rad(self.config['relative_plate_PA_angle'])
+        for count,airmass in enumerate(airmasses): #for each airmass, calculate AD shift
+            shift_vals=disp_utils.diff_shift(wavelengths,airmass,guide_waveref,self.config)  
+            airmass_shifts=[]
+
+            for i in range(0,len(shift_vals)):
+                x=(shift_vals[i])*np.sin(raw_para_angles[count])-centre_shift*np.sin(centring_q)
+                y=(shift_vals[i])*np.cos(raw_para_angles[count])-centre_shift*np.cos(centring_q)
+                airmass_shifts.append([x*np.cos(phi)-y*np.sin(phi),y*np.cos(phi)+x*np.sin(phi)])
+                
+            shifts_para.append(airmass_shifts)
+
+        self.output['shifts']=np.array(shifts_para)
+        centre_shift_para=[-centre_shift*np.sin(centring_q),-centre_shift*np.cos(centring_q)]
+        centre_shift_para=[centre_shift_para[0]*np.cos(phi)-centre_shift_para[1]*np.sin(phi),
+                           centre_shift_para[1]*np.cos(phi)+centre_shift_para[0]*np.sin(phi)]
+        self.output['centre_shift']=centre_shift_para
+    
+    def load_PSFs(self):
+        """
+        Generates the moffat PSFs - one for 
+        """
+        all_PSFs=[]
+       
+        for i in range(0,len(self.output['airmasses'])):
+            PSFs=disp_utils.make_moffat_PSFs(self.output['wavelengths'],self.output['shifts'][i],
+                                                         self.output['airmasses'][i],self.output['major_axis'],self.config)
+            all_PSFs.append(PSFs)
+            
+        self.output['PSFs']=all_PSFs
+        
+    def load_aperture(self,band):
+        """
+        Generates apertures, one for each fibre in the bundle
+        """
+        apertures=disp_utils.make_aperture(band,self.output['major_axis'],self.config['sim_scale'])
+        self.output['apertures']=apertures
+           
+    def calculate_transmissions(self):
+        """
+        Calculates the transmission of the PSFs through the apertures.
+        """
+        apertures_resized = np.repeat(self.output['apertures'][:,np.newaxis], np.shape(self.output['PSFs'])[1], axis=1)
+        apertures_resized = np.repeat(apertures_resized[:,np.newaxis], np.shape(self.output['PSFs'])[0], axis=1)
+
+        PSFs_through_apertures=apertures_resized*self.output['PSFs']
+        self.output['PSFs_through_apertures']=PSFs_through_apertures
+        
+        transmissions=np.sum(PSFs_through_apertures,axis=(-1,-2))
+        self.output['raw_transmissions']=transmissions
+        
+        integration_transmissions=np.mean(transmissions,axis=1)
+        self.output['raw_integration_transmissions']=integration_transmissions #collapsed along airmass axis
+        
+    def plots(self):
+        """
+        Function to illustrate simulation
+        1) Transmission vs wavelength curves for individual fibres and entire bundle
+        2) Track plot of monochromatic spot PSFs on the aperture over an integration
+        """
+        fig,ax=plt.subplots(figsize=[7,5])
+        plt.ylim(0,0.1)
+        ax.set_ylabel("Individual Fibre Transmission")
+        for fibre_trans in self.output['raw_integration_transmissions']:
+            ax.plot(self.output['wavelengths'],fibre_trans,color='black',linewidth=0.5) 
+        ax2=ax.twinx()
+        ax2.axhline(0,label="Individual Fibre",color='black',linewidth=0.5)
+        ax2.plot(self.output['wavelengths'],np.sum(self.output['raw_integration_transmissions'],axis=0),label="Bundle = {}um".format(self.input['aperture_waveref']),color='red')
+        plt.axvline(self.input['guide_waveref'],label="Guide = {}um".format(self.input['guide_waveref']),color='black',linestyle='--',linewidth=0.5)
+        ax2.set_ylabel("Bundle Transmission")
+        ax2.set_ylim(0,1)
+        ax.set_xlabel("Wavelength (um)")
+        plt.legend()
+        
+        fig, ax = plt.subplots(figsize=[5,5]) 
+        weights = np.linspace(0, len(self.output['wavelengths'])-1,4)
+        norm = mpl.colors.Normalize(vmin=min(weights), vmax=max(weights))
+        cmap = mpl.cm.ScalarMappable(norm=norm, cmap='seismic')
+        circle1 = plt.Circle((0, 0), self.output['major_axis']/2, color='black', fill=False, label='~Aperture')
+        ax.add_patch(circle1)    
+        plt.axvline(0,color='black',linestyle='--',linewidth=0.7,label="PA = {}".format(self.config['relative_plate_PA_angle']))
+        plt.scatter(self.output['centre_shift'][0],self.output['centre_shift'][1],label='Guide = {}um'.format(self.input['guide_waveref']),color='black',marker='+')
+        plt.xlim(-0.4,0.4)
+        plt.ylim(-0.4,0.4)
+        shifts=self.output['shifts']
+        for i in weights:
+            xs,ys=[],[]
+            for o in range(0,len(shifts)):
+                xs.append(shifts[o][int(i)][0])
+                ys.append(shifts[o][int(i)][1]) 
+            plt.plot(xs,ys,marker='x',color=cmap.to_rgba(int(i)),label="{}um".format(round(self.output['wavelengths'][int(i)],4)))
+        plt.legend()
+        plt.xlabel("x (arcsec)")
+        plt.ylabel("y (arcsec)")
+            
+    def run(self,HA_start,HA_end,declination,band,sampling=0.01,guide_waveref=-1,aperture_waveref=-1):
+        """
+        Function to run MOSAIC AD simulation for individual fibre transmission curves
+        INPUTS:
+        HA_start,HA_end: float, hours
+            HA values to start and end the simulated observation at
+        declination: float, degrees
+            Declination of the target in the simulated observation
+        band: string
+            Which MOSAIC observing band to carry out the simulation in.
+            Must be in the form "X_X_X", such as "LR_VIS_B" with the following options:
+            
+            {LR: {VIS: {B, G, R, All}, NIR: {IY, J, H, All}}, HR: {VIS: {G, R}, NIR: {IY, H}}} 
+            
+            *HR not currently implemented*
+            
+        sampling: float, um
+            What interval in um to sample the wavelengths at, default 0.01um
+        guide_waveref, aperture_waveref: float, um
+            What wavelength to guide the telescope on and centre the apertures on halfway through the integration respectively
+            defaults (when values set to -1) are wavelengths halfway through the chosen band 
+        
+        RETURNS:
+        wavelengths: 1D array of floats, um
+            wavelength samples the transmissions have been calculated for
+        fibre_transmissions: 2D array of floats, fraction
+            transmissions of each of the fibres for each different simulated wavelength.
+            first axis is the respective fibre, second axis is the wavelength
+        """
+        self.load_HA(HA_start,HA_end,declination)
+        self.load_MOSAIC_band(band,sampling)
+        
+        if guide_waveref==-1 or aperture_waveref ==-1:
+            band_mid=round((self.output['wavelengths'][0]+self.output['wavelengths'][-1])/2,3)
+            guide_waveref,aperture_waveref=band_mid,band_mid
+        self.input['guide_waveref']=guide_waveref
+        self.input['aperture_waveref']=aperture_waveref
+        self.calculate_shifts(guide_waveref,aperture_waveref)
+        
+        self.load_aperture(band)
+        self.load_PSFs()
+        self.calculate_transmissions()
+        
+        wavelengths=self.output['wavelengths']
+        fibre_transmissions=self.output['raw_integration_transmissions'] 
+        
+        return wavelengths, fibre_transmissions
--- scopesim/effects/atmo_disp_shifts_trans_utils.py
+++ scopesim/effects/atmo_disp_shifts_trans_utils.py
@@ -1,25 +1,18 @@
-
 import math
 import numpy as np
 
-from configobj import ConfigObj
-
 from astropy.modeling.models import Moffat2D
-from astropy.modeling.functional_models import Disk2D
-
-import matplotlib.pyplot as plt
 
 from astropy.coordinates import Angle
 from astropy import units as u
+from matplotlib.path import Path
 
-def calculate_FWHM(wavelength,airmass):
-    Config = ConfigObj('conf.ini')
-
-    D = float(Config['Parameters']['telescope_diameter']) 
-    L0 = float(Config['Parameters']['wavefront_outer_scale'])
-    median_seeing = float(Config['Seeing']['median_seeing'])
-    median_seeing_wl = float(Config['Seeing']['median_seeing_wl'])
-       
+def calculate_FWHM(wavelength,airmass,config):
+    median_seeing=config['median_seeing']
+    median_seeing_wl=config['median_seeing_wl']
+    L0=config['wavefront_outerscale']
+    D=config['telescope_diameter']
+    
     r0=0.1*median_seeing**(-1)*(wavelength/median_seeing_wl)**1.2*airmass**(-0.6)  
     F_kolb=1/(1+300*(D/L0))-1
     
@@ -30,52 +23,42 @@ def calculate_FWHM(wavelength,airmass):
     
     return FWHM_total
 
-def make_moffat_PSFs(wavelengths,offsets,airmass,diameter,beta=2.5):
+def make_moffat_PSFs(wavelengths,offsets,airmass,diameter,config,beta=2.5):
+    scale=config['sim_scale']
     
     wavelengths,offsets = np.array(wavelengths),np.array(offsets)
-    Config = ConfigObj('conf.ini')
-    scale = float(Config['Sim_Parameters']['scale'])
-     
     boundary=math.ceil(diameter/2/scale) #radius of aperture in pixels
 
-    FWHMs = calculate_FWHM(wavelengths,airmass)
+    FWHMs = calculate_FWHM(wavelengths,airmass,config)
 
     x = np.arange(-boundary, boundary+1)
     y = np.arange(-boundary, boundary+1)
     x, y = np.meshgrid(x, y)
           
     PSFs=np.zeros((len(wavelengths),boundary*2+1,boundary*2+1))
-    total_vals=[]
-
     for count in range(0,len(wavelengths)):
         alpha=FWHMs[count]/scale/(2*np.sqrt(2**(1/beta)-1))
+        moffat_total=(np.pi*alpha**2)/(beta-1)
         x_pos=offsets[count][0]/scale  
         y_pos=offsets[count][1]/scale
 
         Moffat=Moffat2D(1,x_pos,y_pos,alpha,beta)
         Moffat_data=Moffat(x,y)
-  
-        PSFs[count]=Moffat_data
-        
-        moffat_total=(np.pi*alpha**2)/(beta-1)
-        total_vals.append(moffat_total) 
+        PSFs[count]=Moffat_data/moffat_total
 
-    return PSFs,total_vals
+    return PSFs
 
 def parallatic_angle(HA,dec,lat):
     HA=Angle(HA*u.hour).rad
     q = np.arctan2(np.sin(HA),(np.cos(dec)*np.tan(lat)-np.sin(dec)*np.cos(HA)))
     return q
 
-def diff_shift(wave, airmass, atm_ref_wav):
+def diff_shift(wave, airmass, atm_ref_wav,config):
+    T = config['temperature']
+    HR = config['humidity']
+    P = config['pressure']
     Lambda0 = atm_ref_wav
     wave = wave
-    
-    config=ConfigObj('conf.ini')
-
-    T = float(config["Environment"]["temperature"])+273.15
-    HR = float(config["Environment"]["humidity"])/100
-    P = float(config["Environment"]["pressure"])
     
     ZD_deg = airmass_to_zenith_dist(airmass)
     ZD = np.deg2rad(ZD_deg)
@@ -90,10 +73,8 @@ def diff_shift(wave, airmass, atm_ref_wav):
 
     #dry air density
     Da = (1 + Pa * (57.90*1.0e-8 - 0.0009325/T + 0.25844/T**2)) * Pa/T
-
-    #1 - P instead of 1 + Pa here? Why? Makes minimal affect of actual values...
     
-    #water vapour density ?
+    #water vapour density
     Dw = (1 + Pw * (1 + 3.7 * 1E-4 * Pw) * (- 2.37321 * 1E-3 + 2.23366/T
                                             - 710.792/T**2
                                             + 77514.1/T**3)) * Pw/T
@@ -114,108 +95,51 @@ def diff_shift(wave, airmass, atm_ref_wav):
 def airmass_to_zenith_dist(airmass):
     return np.rad2deg(np.arccos(1/airmass))
 
-def make_aperture(type,major_axis,hex_rotation=0):
-    config=ConfigObj('conf.ini')
-    scale=float(config['Sim_Parameters']['scale'])
-    boundary=math.ceil(major_axis/2/scale) #radius of aperture in pixels
-    if type == "circle":
-    
-        x = np.arange(-boundary, boundary+1)
-        y = np.arange(-boundary, boundary+1)
-        x, y = np.meshgrid(x, y)
- 
-        Disk=Disk2D(1,0,0,major_axis/2/scale)
-        aperture=Disk(x,y)    
-
-        return aperture
-    
-    if type == "hexagons":
-        sampling = major_axis/3/scale
-        aperture_array=np.zeros([boundary*2+1,boundary*2+1])
-
-        triangle_side=sampling*np.sqrt(3)/3
-        core = 2 * triangle_side * np.cos(np.pi/4)
-        aperture_centre=[boundary,boundary]
-        alpha = hex_rotation
-        
-        centre_0=aperture_centre
-        centre_1=[centre_0[0]+sampling*np.cos(np.pi/2-alpha),centre_0[1]+sampling-sampling*(1-np.sin(np.pi/2-alpha))]
-        centre_2=[centre_0[0]+np.sqrt((triangle_side*3/2)**2+(sampling/2)**2)*np.cos(np.pi/6-alpha),centre_0[1]+np.sqrt((triangle_side*3/2)**2+(sampling/2)**2)*np.sin(np.pi/6-alpha)]
-        centre_3=[centre_0[0]+np.sqrt((triangle_side*3/2)**2+(sampling/2)**2)*np.cos(np.pi/6+alpha),centre_0[1]-np.sqrt((triangle_side*3/2)**2+(sampling/2)**2)*np.sin(np.pi/6+alpha)]
-        centre_4=[centre_0[0]-sampling*np.cos(np.pi/2-alpha),centre_0[1]-sampling+sampling*(1-np.sin(np.pi/2-alpha))]
-        centre_5=[centre_0[0]-np.sqrt((triangle_side*3/2)**2+(sampling/2)**2)*np.cos(np.pi/6-alpha),centre_0[1]-np.sqrt((triangle_side*3/2)**2+(sampling/2)**2)*np.sin(np.pi/6-alpha)]
-        centre_6=[centre_0[0]-np.sqrt((triangle_side*3/2)**2+(sampling/2)**2)*np.cos(np.pi/6+alpha),centre_0[1]+np.sqrt((triangle_side*3/2)**2+(sampling/2)**2)*np.sin(np.pi/6+alpha)]
-
-        centres=[centre_0,centre_1,centre_2,centre_3,centre_4,centre_5,centre_6]
-
-        for centre in centres:
-            if alpha == 0:
-                P1=[centre[0]+triangle_side*np.cos(np.pi*1/3-alpha),centre[1]+triangle_side*np.sin(np.pi/3-alpha)]
-                P2=[centre[0]+triangle_side*np.cos(alpha),centre[1]-triangle_side*np.sin(alpha)]
-                P3=[centre[0]+triangle_side*np.cos(np.pi/3+alpha),centre[1]-triangle_side*np.sin(np.pi/3+alpha)]
-                P4=[centre[0]-triangle_side*np.cos(np.pi/3-alpha),centre[1]-triangle_side*np.sin(np.pi/3-alpha)]
-                P5=[centre[0]-triangle_side*np.cos(-alpha),centre[1]-triangle_side*np.sin(-alpha)]
-                P6=[centre[0]-triangle_side*np.cos(np.pi/3+alpha),centre[1]+triangle_side*np.sin(np.pi/3+alpha)]
-
-                L12_m,L12_c=line(P1,P2)
-                L23_m,L23_c=line(P2,P3)
-                L34_m,L34_c=line(P3,P4)
-                L45_m,L45_c=line(P4,P5)
-                L56_m,L56_c=line(P5,P6)
-                L61_m,L61_c=line(P6,P1)
-                           
-                for y in range(0,len(aperture_array)):
-                    for x in range(0,len(aperture_array)):           
-                        if x < centre_0[0] + triangle_side * 2 and x > centre_0[0] - triangle_side * 2 and y < centre_0[1] + sampling and y > centre_0[1] - sampling: 
-                            aperture_array[y][x]=1         
-                        elif y < L61_m*x + L61_c and y > L34_m*x + L34_c and y < L12_m*x + L12_c and y > L23_m*x + L23_c and y > L45_m*x + L45_c and y < L56_m*x + L56_c:
-                            aperture_array[y][x]=1  
-                            
-            elif alpha != np.pi/6 and alpha != -np.pi/6:
-                P1=[centre[0]+triangle_side*np.cos(np.pi*1/3-alpha),centre[1]+triangle_side*np.sin(np.pi/3-alpha)]
-                P2=[centre[0]+triangle_side*np.cos(alpha),centre[1]-triangle_side*np.sin(alpha)]
-                P3=[centre[0]+triangle_side*np.cos(np.pi/3+alpha),centre[1]-triangle_side*np.sin(np.pi/3+alpha)]
-                P4=[centre[0]-triangle_side*np.cos(np.pi/3-alpha),centre[1]-triangle_side*np.sin(np.pi/3-alpha)]
-                P5=[centre[0]-triangle_side*np.cos(-alpha),centre[1]-triangle_side*np.sin(-alpha)]
-                P6=[centre[0]-triangle_side*np.cos(np.pi/3+alpha),centre[1]+triangle_side*np.sin(np.pi/3+alpha)]
-
-                L12_m,L12_c=line(P1,P2)
-                L23_m,L23_c=line(P2,P3)
-                L34_m,L34_c=line(P3,P4)
-                L45_m,L45_c=line(P4,P5)
-                L56_m,L56_c=line(P5,P6)
-                L61_m,L61_c=line(P6,P1)
-                
-                for y in range(0,len(aperture_array)):
-                    for x in range(0,len(aperture_array)):
-                        if y > centre_0[1] - core and y < centre_0[1] + core and x > centre_0[1] - core and x < centre_0[1] + core:
-                            aperture_array[y][x]=1
-                        elif y < L61_m*x + L61_c and y > L34_m*x + L34_c and y < L12_m*x + L12_c and y > L23_m*x + L23_c and y > L45_m*x + L45_c and y < L56_m*x + L56_c:
-                            aperture_array[y][x]=1
-                                
-            elif alpha == np.pi/6 or alpha == - np.pi/6:
-                P1=[centre[0]+triangle_side*np.cos(np.pi/3-alpha),centre[1]+triangle_side*np.sin(np.pi/3-alpha)]
-                P2=[centre[0]+triangle_side*np.cos(alpha),centre[1]-triangle_side*np.sin(alpha)]
-                P3=[centre[0]+triangle_side*np.cos(np.pi/3+alpha),centre[1]-triangle_side*np.sin(np.pi/3+alpha)]
-                P4=[centre[0]-triangle_side*np.cos(np.pi/3-alpha),centre[1]-triangle_side*np.sin(np.pi/3-alpha)]
-                P5=[centre[0]-triangle_side*np.cos(-alpha),centre[1]-triangle_side*np.sin(-alpha)]
-                P6=[centre[0]-triangle_side*np.cos(np.pi/3+alpha),centre[1]+triangle_side*np.sin(np.pi/3+alpha)]
-                
-                L23_m,L23_c=line(P2,P3)
-                L34_m,L34_c=line(P3,P4)
-                L56_m,L56_c=line(P5,P6)
-                L61_m,L61_c=line(P6,P1)
-                
-                for y in range(0,len(aperture_array)):
-                    for x in range(0,len(aperture_array)):
-                        if y < centre_0[1] + triangle_side * 2 and y > centre_0[1] - triangle_side * 2 and x < centre_0[0] + sampling and x > centre_0[0] - sampling: 
-                            aperture_array[y][x]=1    
-                        elif y < L61_m*x + L61_c and y > L34_m*x + L34_c and y > L23_m*x + L23_c and  y < L56_m*x + L56_c and x > centre[0] - sampling/2 and x < centre[0] + sampling/2:
-                            aperture_array[y][x]=1   
-                             
-        return aperture_array
-    
 def line(A,B):
     m=(A[1]-B[1])/(A[0]-B[0])
     c=A[1]-m*A[0] 
     return m,c
+
+def make_aperture(band,major_axis,scale):
+    scale=scale
+    boundary=math.ceil(major_axis/2/scale) #radius of aperture in pixels
+
+    if band[0:6]=="LR_VIS" or band[0:6]=="LR_NIR" or band[0:6]=="HR_NIR":
+        sampling = major_axis/3/scale
+        
+        triangle_side=sampling*np.sqrt(3)/3
+        aperture_centre=[boundary,boundary]
+        
+        centre_0=aperture_centre
+        centre_1=[centre_0[0],centre_0[1]+sampling]
+        centre_2=[centre_0[0]+np.sqrt((triangle_side*3/2)**2+(sampling/2)**2)*np.cos(np.pi/6),centre_0[1]+np.sqrt((triangle_side*3/2)**2+(sampling/2)**2)*np.sin(np.pi/6)]
+        centre_3=[centre_0[0]+np.sqrt((triangle_side*3/2)**2+(sampling/2)**2)*np.cos(np.pi/6),centre_0[1]-np.sqrt((triangle_side*3/2)**2+(sampling/2)**2)*np.sin(np.pi/6)]
+        centre_4=[centre_0[0],centre_0[1]-sampling]
+        centre_5=[centre_0[0]-np.sqrt((triangle_side*3/2)**2+(sampling/2)**2)*np.cos(np.pi/6),centre_0[1]-np.sqrt((triangle_side*3/2)**2+(sampling/2)**2)*np.sin(np.pi/6)]
+        centre_6=[centre_0[0]-np.sqrt((triangle_side*3/2)**2+(sampling/2)**2)*np.cos(np.pi/6),centre_0[1]+np.sqrt((triangle_side*3/2)**2+(sampling/2)**2)*np.sin(np.pi/6)]
+
+        centres=[centre_0,centre_1,centre_2,centre_3,centre_4,centre_5,centre_6]
+
+        apertures=np.zeros((7,boundary*2+1,boundary*2+1))
+        for count,centre in enumerate(centres):
+            P1=[centre[0]+triangle_side*np.cos(np.pi*1/3),centre[1]+triangle_side*np.sin(np.pi/3)]
+            P2=[centre[0]+triangle_side,centre[1]]
+            P3=[centre[0]+triangle_side*np.cos(np.pi/3),centre[1]-triangle_side*np.sin(np.pi/3)]
+            P4=[centre[0]-triangle_side*np.cos(np.pi/3),centre[1]-triangle_side*np.sin(np.pi/3)]
+            P5=[centre[0]-triangle_side,centre[1]]
+            P6=[centre[0]-triangle_side*np.cos(np.pi/3),centre[1]+triangle_side*np.sin(np.pi/3)]
+
+            polygon=[P1,P2,P3,P4,P5,P6]
+            height=boundary*2+1
+            width=boundary*2+1
+            poly_path=Path(polygon)
+
+            x, y = np.mgrid[:height, :width]
+            coors=np.hstack((x.reshape(-1, 1), y.reshape(-1,1))) # coors.shape is (4000000,2)
+
+            mask = poly_path.contains_points(coors)
+            mask=mask.reshape(height, width)
+            
+            apertures[count]=mask
+            
+    return apertures
