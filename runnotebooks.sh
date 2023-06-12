49cc7f750eabd0737e32f91037c05c9522b01b75
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2023-06-12 09:31:55 +0200

    Optionally checkout IRDB when running notebooks
    This prevents the IRDB from being downloaded from the university
    servers, because that download too often fails.

--- runnotebooks.sh
+++ runnotebooks.sh
@@ -1,5 +1,25 @@
 #!/usr/bin/env bash
 
+if [[ "${1}x" == "--clone-irdbx" ]] ; then
+  # Cloning IRDB
+  if [[ ! -e irdb ]] ; then
+    git clone https://github.com/AstarVienna/irdb.git
+  fi
+
+  # https://github.com/koalaman/shellcheck/wiki/SC2044
+  find . -iname "*.ipynb" -printf '%h\0' | sort -z | uniq -z | while IFS= read -r -d '' dirnotebooks; do
+    echo "${dirnotebooks}"
+    dirinstpkgs="${dirnotebooks}/inst_pkgs"
+    if [[ (! -e ./docs/source/examples/inst_pkgs) && (! -L ./docs/source/examples/inst_pkgs) ]] ; then
+      echo "Cretaing symlink to irdb: ${dirinstpkgs}"
+      ln -s irdb "${dirinstpkgs}"
+    else
+      echo "Dericetory exists, not creating symlink: ${dirinstpkgs}"
+    fi
+  done
+fi
+
+
 # https://github.com/koalaman/shellcheck/wiki/SC2044
 find . -iname "*.ipynb" -print0 | while IFS= read -r -d '' fnnotebook
 do
