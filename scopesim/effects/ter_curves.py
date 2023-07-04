dc4a4347eed0443fd7e6433a6ccafb0641b07857
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2023-07-04 07:02:28 +0200

    Simplify PupilTransmission

--- scopesim/effects/ter_curves.py
+++ scopesim/effects/ter_curves.py
@@ -830,13 +830,6 @@ class PupilTransmission(TERCurve):
         self.params = {"wave_min": "!SIM.spectral.wave_min",
                        "wave_max": "!SIM.spectral.wave_max"}
         self.params.update(kwargs)
-        self.make_ter_curve(transmission)
-
-    def update_transmission(self, transmission, **kwargs):
-        self.params.update(kwargs)
-        self.make_ter_curve(transmission)
-
-    def make_ter_curve(self, transmission):
         wave_min = from_currsys(self.params["wave_min"]) * u.um
         wave_max = from_currsys(self.params["wave_max"]) * u.um
         transmission = from_currsys(transmission)
@@ -844,6 +837,9 @@ class PupilTransmission(TERCurve):
         super().__init__(wavelength=[wave_min, wave_max],
                          transmission=[transmission, transmission],
                          emissivity=[0., 0.], **self.params)
+
+    def update_transmission(self, transmission, **kwargs):
+        self.__init__(transmission, **kwargs)
 
 
 class ADCWheel(Effect):
