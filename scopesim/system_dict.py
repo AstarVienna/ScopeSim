d1746a638efd48971324a4c74de6a1130fbe06c5
teutoburg <ghost@instruct.at>
2023-07-07 02:04:33 +0200

    Rewrite SystemDict to be able to inherit from MutableMapping.
    Also fix __repr__ and __str__, make "pretty str"-method IO-stream based.

--- scopesim/system_dict.py
+++ scopesim/system_dict.py
@@ -1,17 +1,25 @@
+# -*- coding: utf-8 -*-
+
 import logging
+from typing import TextIO
+from io import StringIO
+from collections.abc import Iterable, Mapping, MutableMapping
+
+from more_itertools import ilen
 
 
-class SystemDict():
+class SystemDict(MutableMapping):
     def __init__(self, new_dict=None):
         self.dic = {}
-        if isinstance(new_dict, dict):
+        if isinstance(new_dict, Mapping):
             self.update(new_dict)
-        elif isinstance(new_dict, list):
+        elif isinstance(new_dict, Iterable):
             for entry in new_dict:
                 self.update(entry)
 
-    def update(self, new_dict):
-        if isinstance(new_dict, dict) \
+    def update(self, new_dict: MutableMapping) -> None:
+        # TODO: why do we check for dict here but not in the else?
+        if isinstance(new_dict, Mapping) \
                 and "alias" in new_dict \
                 and "properties" in new_dict:
             alias = new_dict["alias"]
@@ -35,6 +43,8 @@ class SystemDict():
 
     def __getitem__(self, item):
         if isinstance(item, str) and item.startswith("!"):
+            # TODO: these should be replaced with item.removeprefix("!")
+            #       once we can finally drop support for Python 3.8 UwU
             item_chunks = item[1:].split(".")
             entry = self.dic
             for item in item_chunks:
@@ -44,45 +54,88 @@ class SystemDict():
 
     def __setitem__(self, key, value):
         if isinstance(key, str) and key.startswith("!"):
-            key_chunks = key[1:].split(".")
+            # TODO: these should be replaced with item.removeprefix("!")
+            #       once we can finally drop support for Python 3.8 UwU
+            *key_chunks, final_key = key[1:].split(".")
             entry = self.dic
-            for key in key_chunks[:-1]:
+            for key in key_chunks:
                 if key not in entry:
                     entry[key] = {}
                 entry = entry[key]
-            entry[key_chunks[-1]] = value
+            entry[final_key] = value
         else:
             self.dic[key] = value
 
-    def __contains__(self, item):
-        if isinstance(item, str) and item.startswith("!"):
-            item_chunks = item[1:].split(".")
-            entry = self.dic
-            for item in item_chunks:
-                if not isinstance(entry, dict) or item not in entry:
-                    return False
-                entry = entry[item]
-            return True
-        return item in self.dic
+    def __delitem__(self, item):
+        raise NotImplementedError("item deletion is not yet implemented for "
+                                  f"{self.__class__.__name__}")
 
-    def __repr__(self):
-        msg = "<SystemDict> contents:"
-        for key, val in self.dic.items():
-            msg += f"\n{key}: "
-            if isinstance(val, dict):
-                for subkey in val.keys():
-                    msg += f"\n  {subkey}: {val[subkey]}"
+    # def __contains__(self, item):
+          # method is redundant when inheriting from abc
+    #     if isinstance(item, str) and item.startswith("!"):
+    #         # TODO: these should be replaced with item.removeprefix("!")
+    #         #       once we can finally drop support for Python 3.8 UwU
+    #         item_chunks = item[1:].split(".")
+    #         entry = self.dic
+    #         for item in item_chunks:
+    #             if not isinstance(entry, Mapping) or item not in entry:
+    #                 return False
+    #             entry = entry[item]
+    #         return True
+    #     return item in self.dic
+
+    def _yield_subkeys(self, key, value):
+        for subkey, subvalue in value.items():
+            if isinstance(subvalue, Mapping):
+                yield from self._yield_subkeys(f"{key}.{subkey}", subvalue)
             else:
-                msg += f"{val}\n"
-        return msg
+                yield f"!{key}.{subkey}"
+
+    def __iter__(self):
+        for key, value in self.dic.items():
+            if isinstance(value, Mapping):
+                yield from self._yield_subkeys(key, value)
+            else:
+                yield key
+
+    def __len__(self) -> int:
+        return ilen(iter(self))
+
+    def _write_subdict(self, subdict: Mapping, stream: TextIO,
+                       pad: str = "") -> None:
+        pre = pad.replace("├─", "│ ").replace("└─", "  ")
+        n_sub = len(subdict)
+        for i_sub, (key, val) in enumerate(subdict.items()):
+            subpre = "└─" if i_sub == n_sub - 1 else "├─"
+            stream.write(f"{pre}{subpre}{key}: ")
+            if isinstance(val, Mapping):
+                self._write_subdict(val, stream, pre + subpre)
+            else:
+                stream.write(f"{val}")
+
+    def write_string(self, stream: TextIO) -> None:
+        """Write formatted string representation to I/O stream"""
+        stream.write("SystemDict contents:")
+        self._write_subdict(self.dic, stream, "\n")
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.dic!r})"
+
+    def __str__(self) -> str:
+        # SystemDict({"foo":5, "bar":{"bogus": {"a":42, "b":69}, "baz":"meh"}, "moo":"yolo", "yeet": {"x":0, "y":420}})
+        # "SystemDict contents:\n├─foo: 5\n├─bar: \n│ ├─bogus: \n│ │ ├─a: 42\n│ │ └─b: 69\n│ └─baz: meh\n├─moo: yolo\n└─yeet: \n  ├─x: 0\n  └─y: 420"
+        with StringIO() as str_stream:
+            self.write_string(str_stream)
+            output = str_stream.getvalue()
+        return output
 
 
-def recursive_update(old_dict, new_dict):
+def recursive_update(old_dict: MutableMapping, new_dict: Mapping) -> MutableMapping:
     if new_dict is not None:
         for key in new_dict:
             if old_dict is not None and key in old_dict:
-                if isinstance(old_dict[key], dict):
-                    if isinstance(new_dict[key], dict):
+                if isinstance(old_dict[key], Mapping):
+                    if isinstance(new_dict[key], Mapping):
                         old_dict[key] = recursive_update(old_dict[key],
                                                          new_dict[key])
                     else:
@@ -90,7 +143,7 @@ def recursive_update(old_dict, new_dict):
                                         old_dict[key], new_dict[key])
                         old_dict[key] = new_dict[key]
                 else:
-                    if isinstance(new_dict[key], dict):
+                    if isinstance(new_dict[key], Mapping):
                         logging.warning("Overwriting non-dict: %s with dict: %s",
                                         old_dict[key], new_dict[key])
                     old_dict[key] = new_dict[key]
