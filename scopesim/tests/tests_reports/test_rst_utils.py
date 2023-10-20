6dbfca5d167bdbc605ab717fa4f0c582fdc79ae1
teutoburg <ghost@instruct.at>
2023-10-20 23:20:02 +0200

    Attempt to fix report tests

--- scopesim/tests/tests_reports/test_rst_utils.py
+++ scopesim/tests/tests_reports/test_rst_utils.py
@@ -1,35 +1,45 @@
 import pytest
+from unittest.mock import patch
+
+import os
 from pathlib import Path
 import shutil
 from docutils.core import publish_doctree
 
 from scopesim.reports import rst_utils as ru
 from scopesim.tests.mocks.py_objects import report_objects as ro
-from scopesim import rc
-rc.__config__["!SIM.reports.image_path"] = "./images_temp/"
-rc.__config__["!SIM.reports.latex_path"] = "./latex_temp/"
-rc.__config__["!SIM.reports.rst_path"] = "./rst_temp/"
+from scopesim.tests.mocks.py_objects import effects_objects as eo
+
 
 CLEAN_UP = True
 PLOTS = False
 
-IMG_PATH = Path(rc.__config__["!SIM.reports.image_path"])
-LATEX_PATH = Path(rc.__config__["!SIM.reports.latex_path"])
-RST_PATH = Path(rc.__config__["!SIM.reports.rst_path"])
+IMG_PATH = Path(__file__).parent / "images_temp"
+LATEX_PATH = Path(__file__).parent / "latex_temp"
+RST_PATH = Path(__file__).parent / "rst_temp"
 
 
-def setup_module():
-    for path in [IMG_PATH, LATEX_PATH, RST_PATH]:
-        if not path.exists():
-            path.mkdir()
-    rc.__config__["!SIM.reports.image_path"] = str(IMG_PATH)
-    rc.__config__["!SIM.reports.latex_path"] = str(LATEX_PATH)
-    rc.__config__["!SIM.reports.rst_path"] = str(RST_PATH)
+@pytest.fixture(scope="module", autouse=True)
+def setup_and_teardown():
+    paths = [IMG_PATH, LATEX_PATH, RST_PATH]
+    for path in paths:
+        path.mkdir(parents=True, exist_ok=True)
 
+    patched = {"!SIM.reports.image_path": str(IMG_PATH.absolute()),
+               "!SIM.reports.latex_path": str(LATEX_PATH.absolute()),
+               "!SIM.reports.rst_path": str(RST_PATH.absolute()),
+               }
+    with patch.dict("scopesim.rc.__config__", patched):
+        # FIXME: remove the second part of this asap
+        try:
+            with patch.dict("scopesim.rc.__currsys__", patched):
+                yield
+        except KeyError:
+            with patch.dict("scopesim.rc.__currsys__.cmds", patched):
+                yield
 
-def teardown_module():
-    for path in [IMG_PATH, LATEX_PATH, RST_PATH]:
-        if CLEAN_UP:
+    if CLEAN_UP:
+        for path in paths:
             shutil.rmtree(path)
 
 
@@ -41,18 +51,18 @@ class TestWalk:
 
 
 class TestPlotRstText:
-    @pytest.mark.skip(reason=("This produces a DeprecationWarning about a "
-                              "module called py23. Find out what that is and "
-                              "remove/replace it."))
+    # @pytest.mark.skip(reason=("This produces a DeprecationWarning about a "
+    #                           "module called py23. Find out what that is and "
+    #                           "remove/replace it."))
     def test_image_file_exists_for_comment_node(self):
         assert IMG_PATH.exists()
         ru.plotify_rst_text(ro.comment_plot_snippet)
         assert (IMG_PATH / "my_fug.png").exists()
         assert (IMG_PATH / "my_fug.pdf").exists()
 
-    @pytest.mark.skip(reason=("This produces a DeprecationWarning about a "
-                              "module called py23. Find out what that is and "
-                              "remove/replace it."))
+    # @pytest.mark.skip(reason=("This produces a DeprecationWarning about a "
+    #                           "module called py23. Find out what that is and "
+    #                           "remove/replace it."))
     def test_image_file_exists_for_comment_node_with_escapable_name(self):
         """Test whether images are created with escapable names.
 
@@ -65,7 +75,6 @@ class TestPlotRstText:
         assert (IMG_PATH / "ty_fug.pdf").exists()
 
     def test_image_file_exists_for_literal_node(self):
-        print(IMG_PATH)
         ru.plotify_rst_text(ro.literal_plot_snippet)
         assert (IMG_PATH / "my_fug3.svg").exists()
         assert (IMG_PATH / "my_fug3.png").exists()
@@ -84,18 +93,27 @@ class TestRstifyRstText:
 
 
 @pytest.mark.skip(reason="Ignoring for Github Actions")
+# @pytest.mark.skip(reason=("This produces a DeprecationWarning about a "
+#                           "module called py23. Find out what that is and "
+#                           "remove/replace it."))
 class TestPlotifyRstText:
-    def test_stuff(self):
-        print(IMG_PATH)
+    @pytest.mark.parametrize("fname", ["my_fug_A.pdf",
+                                       "my_fug_B.svg",
+                                       "my_fug_C.png"])
+    def test_stuff(self, fname):
+        # FIXME: find a better solution (monkeypatch?) than this chdir stuff
+        os.chdir(IMG_PATH.parent)
         ru.plotify_rst_text(ro.big_rst_text)
-        fnames = ["my_fug_A.pdf", "my_fug_B.svg", "my_fug_C.png"]
-        for fname in fnames:
-            assert (IMG_PATH / fname).exists()
+        assert (IMG_PATH / fname).exists()
 
 
 class TestEffectReport:
     def test_all_parts_are_created_in_rc_folders(self):
-        from scopesim.tests.mocks.py_objects import effects_objects as eo
+        # patched = {"!SIM.reports.image_path": str(IMG_PATH.absolute()),
+        #            "!SIM.reports.latex_path": str(LATEX_PATH.absolute()),
+        #            "!SIM.reports.rst_path": str(RST_PATH.absolute()),
+        #            }
+        # with patch.dict("scopesim.rc.__currsys__", patched):
         det_list = eo._detector_list()
         rst_text = det_list.report()
         ru.rstify_rst_text(rst_text, title_char="*", filename=det_list.meta["name"])
