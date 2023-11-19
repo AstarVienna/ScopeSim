42289ebf7b71ac29b29cf2c87394640a5d7ed787
teutoburg <ghost@instruct.at>
2023-11-19 21:24:06 +0100

    More summary

--- runnotebooks.sh
+++ runnotebooks.sh
@@ -1,20 +1,25 @@
 #!/usr/bin/env bash
 
+echo "# Running Notebooks Tests" >> $GITHUB_STEP_SUMMARY
+
 if [[ "x${1}" == "x--clone-irdb" ]] ; then
   # Cloning IRDB
   if [[ ! -e irdb ]] ; then
+    echo "_Cloning IRDB_" >> $GITHUB_STEP_SUMMARY
     git clone https://github.com/AstarVienna/irdb.git
   fi
 
+  echo "## Symlinks" >> $GITHUB_STEP_SUMMARY
   # https://github.com/koalaman/shellcheck/wiki/SC2044
   find . -iname "*.ipynb" -printf '%h\0' | sort -z | uniq -z | while IFS= read -r -d '' dirnotebooks; do
     echo "${dirnotebooks}"
+    echo "- ${dirnotebooks}" >> $GITHUB_STEP_SUMMARY
     dirinstpkgs="${dirnotebooks}/inst_pkgs"
     if [[ (! -e ./docs/source/examples/inst_pkgs) && (! -L ./docs/source/examples/inst_pkgs) ]] ; then
-      echo "Creating symlink to irdb: ${dirinstpkgs}"
+      echo "Creating symlink to irdb: ${dirinstpkgs}" >> $GITHUB_STEP_SUMMARY
       ln -s irdb "${dirinstpkgs}"
     else
-      echo "Directory exists, not creating symlink: ${dirinstpkgs}"
+      echo "Directory exists, not creating symlink: ${dirinstpkgs}" >> $GITHUB_STEP_SUMMARY
     fi
 
     # Comment out any download_package[s] in the notebooks.
@@ -30,7 +35,6 @@ echo "## Notebooks tested" >> $GITHUB_STEP_SUMMARY
 find . -iname "*.ipynb" -print0 | while IFS= read -r -d '' fnnotebook
 do
   echo "Testing ${fnnotebook} ..."
-  echo "- ${fnnotebook}" >> $GITHUB_STEP_SUMMARY
   fnpy="${fnnotebook%.ipynb}.py"
 
   # Convert .ipynb file to .py.
@@ -38,6 +42,7 @@ do
 
   # Run the python script and quit on first error.
   python "${fnpy}" || exit 1
+  echo "- ${fnnotebook}" >> $GITHUB_STEP_SUMMARY
 
   # Delete generated files if --delete is specified.
   # By default do not delete any files.
