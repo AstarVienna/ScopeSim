e459f3b86d2982a3c5e2f713df00dc5beb3af565
teutoburg <ghost@instruct.at>
2023-12-05 21:31:12 +0100

    Actually implement shutter

--- scopesim/effects/shutter.py
+++ scopesim/effects/shutter.py
@@ -1,10 +1,24 @@
 # -*- coding: utf-8 -*-
 """Contains the Shutter effect."""
 
+import logging
+
 from . import Effect
+from ..base_classes import ImagePlaneBase
 
 
 class Shutter(Effect):
     """Simulate a closed shutter, useful for dark exposures."""
 
-    pass
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.meta["z_order"] = [799]
+
+    def apply_to(self, obj, **kwargs):
+        """Set all pixels of image plane to zero."""
+        if not isinstance(obj, ImagePlaneBase):
+            return obj
+
+        logging.warning("Shutter is closed, setting all pixels to zero.")
+        obj.data[:] = 0.0
+        return obj
