4d67386acd89e3b5522167d1037f0f0347828178
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2024-01-17 15:45:33 +0100

    Use another method of importing that does lead to coverage

--- scopesim/tests/test_import_all.py
+++ scopesim/tests/test_import_all.py
@@ -1,8 +1,28 @@
 """Test whether all Python files can be imported."""
 import importlib.util
-from pathlib import Path
+import pkgutil
 
 import scopesim
+
+
+def import_submodules(package):
+    """ Import all submodules of a module, recursively, including subpackages
+
+    :param package: package (name or actual module)
+    :type package: str | module
+    :rtype: dict[str, types.ModuleType]
+
+    https://stackoverflow.com/a/25562415
+    """
+    if isinstance(package, str):
+        package = importlib.import_module(package)
+    results = {}
+    for loader, name, is_pkg in pkgutil.walk_packages(package.__path__):
+        full_name = package.__name__ + '.' + name
+        results[full_name] = importlib.import_module(full_name)
+        if is_pkg:
+            results.update(import_submodules(full_name))
+    return results
 
 
 def test_import_all():
@@ -10,8 +30,4 @@ def test_import_all():
 
     This ensures that all files are included in the code coverage.
     """
-    path_scopesim = Path(scopesim.__file__).parent
-    paths_modules = path_scopesim.glob("**/*.py")
-    for pm in paths_modules:
-        spec = importlib.util.spec_from_file_location("module.name", pm)
-        _ = importlib.util.module_from_spec(spec)
+    import_submodules(scopesim)
