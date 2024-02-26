aec9895af2cff832b9e8ffa011b419c6b8394dd6
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2024-02-26 23:17:39 +0100

    Fix AutoExposure and DetectorModePropertiesSetter

--- scopesim/effects/electronic.py
+++ scopesim/effects/electronic.py
@@ -105,9 +105,9 @@ class DetectorModePropertiesSetter(Effect):
 
         self.meta["detector_readout_mode"] = mode_name
         props_dict = self.mode_properties[mode_name]
-        rc.__currsys__["!OBS.detector_readout_mode"] = mode_name
+        self.cmds["!OBS.detector_readout_mode"] = mode_name
         for key, value in props_dict.items():
-            rc.__currsys__[key] = value
+            self.cmds[key] = value
 
         return obj
 
@@ -226,8 +226,8 @@ class AutoExposure(Effect):
             logger.info("Exposure parameters: DIT=%.3f s  NDIT=%d", dit, ndit)
             logger.info("Total exposure time: %.3f s", dit * ndit)
 
-            rc.__currsys__["!OBS.dit"] = dit
-            rc.__currsys__["!OBS.ndit"] = ndit
+            self.cmds["!OBS.dit"] = dit
+            self.cmds["!OBS.ndit"] = ndit
 
         return obj
 
