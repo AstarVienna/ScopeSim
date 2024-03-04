b5eb205e217a4833e54387af047f1c414207a1ca
teutoburg <ghost@instruct.at>
2024-03-04 13:52:53 +0100

    Some refactoring and several minor changes, see below...
    This pickle functionality wasn't used anywhere
    
    Keep it around for now in case I was mistakten.
    To be replaces with a YAML-based solution in the future...
    
    Early raise...
    
    No more np.all

--- scopesim/source/source_utils.py
+++ scopesim/source/source_utils.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+from typing import Optional, Union
 from collections.abc import Iterable
 
 import numpy as np
@@ -6,143 +8,187 @@ from astropy.io import fits
 from astropy.table import Table
 from synphot import SourceSpectrum, Empirical1D, SpectralElement
 
-from ..utils import find_file, quantify, get_logger
+from ..utils import find_file, get_logger
 
 
 logger = get_logger(__name__)
 
 
-def validate_source_input(**kwargs):
-    if "filename" in kwargs and kwargs["filename"] is not None:
-        filename = kwargs["filename"]
-        if find_file(filename) is None:
-            logger.warning("filename was not found: %s", filename)
-
-    if "image" in kwargs and kwargs["image"] is not None:
-        image_hdu = kwargs["image"]
-        if not isinstance(image_hdu, (fits.PrimaryHDU, fits.ImageHDU)):
-            raise ValueError("image must be fits.HDU object with a WCS."
-                             f"{type(image_hdu) = }")
-
-        if len(wcs.find_all_wcs(image_hdu.header)) == 0:
-            logger.warning("image does not contain valid WCS. %s",
-                            wcs.WCS(image_hdu))
-
-    if "table" in kwargs and kwargs["table"] is not None:
-        tbl = kwargs["table"]
-        if not isinstance(tbl, Table):
-            raise ValueError("table must be an astropy.Table object:"
-                             f"{type(tbl) = }")
-
-        if not np.all([col in tbl.colnames for col in ["x", "y", "ref"]]):
-            raise ValueError("table must contain at least column names: "
-                             f"'x, y, ref': {tbl.colnames}")
-
-    return True
-
-
-def convert_to_list_of_spectra(spectra, lam):
-    spectra_list = []
-    if isinstance(spectra, SourceSpectrum):
-        spectra_list += [spectra]
-
-    elif lam is None and\
-            isinstance(spectra, (tuple, list)) and \
-            isinstance(spectra[0], SourceSpectrum):
-        spectra_list += spectra
-
-    elif lam is not None and len(spectra.shape) == 1 and \
-            isinstance(spectra, np.ndarray) and \
-            isinstance(lam, np.ndarray):
-        spec = SourceSpectrum(Empirical1D, points=lam, lookup_table=spectra)
-        spectra_list += [spec]
-
-    elif ((isinstance(spectra, np.ndarray) and
-           len(spectra.shape) == 2) or
-          (isinstance(spectra, (list, tuple)) and
-           isinstance(spectra[0], np.ndarray))) and \
-            isinstance(lam, np.ndarray):
-
-        for sp in spectra:
-            spec = SourceSpectrum(Empirical1D, points=lam, lookup_table=sp)
-            spectra_list += [spec]
-
-    return spectra_list
-
-
-def photons_in_range(spectra, wave_min, wave_max, area=None, bandpass=None):
+def validate_source_input(**kwargs) -> None:
     """
+    Check validity of kwargs passed to ``Source`` object.
+
+    Currently checks for "filename", "image" and "table", raising the
+    exceptions listed below. Additionally logs a warning if no WCS is found in
+    an image, or if a given filename cannot be found.
 
     Parameters
     ----------
-    spectra
-    wave_min
-        [um]
-    wave_max
-        [um]
-    area : Quantity
-        [m2]
-    bandpass : SpectralElement
+    **kwargs : TYPE
+        DESCRIPTION.
 
+    Raises
+    ------
+    TypeError
+        Raised if an image isn't a FITS HDU or a table isn't an astropy Table.
+    ValueError
+        Raised if a table does not contain the minimum required columns.
 
     Returns
     -------
-    counts : u.Quantity array
+    None
 
     """
+    if (filename := kwargs.get("filename")) is not None:
+        if find_file(filename) is None:
+            logger.warning("filename was not found: %s", filename)
 
-    if isinstance(wave_min, u.Quantity):
-        wave_min = wave_min.to(u.Angstrom).value
-    else:
-        wave_min *= 1E4
+    if (image_hdu := kwargs.get("image")) is not None:
+        if not isinstance(image_hdu, (fits.PrimaryHDU, fits.ImageHDU)):
+            raise TypeError(
+                f"Image must be fits.HDU object: {type(image_hdu) = }")
 
-    if isinstance(wave_max, u.Quantity):
-        wave_max = wave_max.to(u.Angstrom).value
-    else:
-        wave_max *= 1E4
+        if not wcs.find_all_wcs(image_hdu.header):
+            logger.warning(
+                "Image does not contain valid WCS. %s", wcs.WCS(image_hdu))
+
+    if (tbl := kwargs.get("table")) is not None:
+        tbl = kwargs["table"]
+        if not isinstance(tbl, Table):
+            raise TypeError(
+                f"Table must be astropy.Table object: {type(tbl) = }")
+
+        if not {"x", "y", "ref"}.issubset(tbl.colnames):
+            raise ValueError(
+                "Table must contain at least the following column names: 'x', "
+                f"""'y', 'ref'; found only: '{"', '".join(tbl.colnames)}'""")
+            # TODO py312: The triple quotes will become redundant in 3.12 !
+
+
+def convert_to_list_of_spectra(spectra, lam) -> list[SourceSpectrum]:
+    """Produce SourceSpectrum instances or pass them through."""
+    def _synphotify(spec):
+        if not isinstance(lam, np.ndarray):
+            raise TypeError("If spectra is/are given as array(s), lam must be "
+                            "an array as well.")
+        return SourceSpectrum(Empirical1D, points=lam, lookup_table=spec)
+
+    def _from_arrays(specarrays):
+        for spec in specarrays:
+            yield _synphotify(spec)
+
+    def _get_list():
+        if isinstance(spectra, SourceSpectrum):
+            yield spectra
+            return
+
+        if (isinstance(spectra, Iterable) and
+                not isinstance(spectra, np.ndarray)):
+            if all(isinstance(spec, SourceSpectrum) for spec in spectra):
+                yield from spectra
+            elif all(isinstance(spec, np.ndarray) for spec in spectra):
+                yield from _from_arrays(spectra)
+            else:
+                raise ValueError(
+                    "If given as an iterable, spectra must consist of all "
+                    "synphot spectra or all arrays")
+            return
+
+        if isinstance(spectra, np.ndarray):
+            if spectra.ndim == 1:
+                yield _synphotify(spectra)
+            elif spectra.ndim == 2:
+                yield from _from_arrays(spectra)
+            else:
+                raise ValueError(
+                    "If given as an array, spectra must have either 1 (single "
+                    "flux list) or 2 (flux of multiple spectra) dimensions, "
+                    f"but {spectra.ndim} were found.")
+            return
+
+    return list(_get_list())
+
+
+# FIXME: typing: This should work with the more general (and true, since
+#        conversion is done anyway) Quantity["length"] (or "area" resp.), but
+#        doing so currently causes a NameError. Not sure what's going on.
+def photons_in_range(
+        spectra: SourceSpectrum,
+        wave_min: Union[u.Quantity[u.um], float],
+        wave_max: Union[u.Quantity[u.um], float],
+        area: Optional[Union[u.Quantity[u.m**2], float]] = None,
+        bandpass: Optional[SpectralElement] = None,
+) -> Union[u.Quantity[u.ph * u.s**-1 * u.m**-2], u.Quantity[u.ph * u.s**-1]]:
+    """
+    Integrate photons from spectrum in given wavelength range.
+
+    Parameters
+    ----------
+    spectra : SourceSpectrum
+        Input spectrum.
+    wave_min : Union[u.Quantity["length"], float]
+        Minimum wavelength. If float, assumes um.
+    wave_max : Union[u.Quantity["length"], float]
+        Maximum wavelength. If float, assumes um.
+    area : Optional[Union[u.Quantity["area"], float]], optional
+        Area to multiply with. If float, assumes m**2. The default is None.
+    bandpass : Optional[SpectralElement], optional
+        Filter to take into account, if any. The default is None.
+
+    Returns
+    -------
+    counts : astropy.units.Quantity
+        Either in ph/s/m**2 or just ph/s (if area was given).
+
+    """
+    # Note: Assuming um if given as float.
+    wave_min = (wave_min << u.um).to(u.Angstrom).value
+    wave_max = (wave_max << u.um).to(u.Angstrom).value
+    # Note: There appear to be some float shenanigans going on here, but
+    # rounding produces an error in the spectrum evaluation. Not sure what's
+    # going on, maybe it's fine as-is.
 
     counts = []
     for spec in spectra:
         waveset = spec.waveset.value
         mask = (waveset > wave_min) * (waveset < wave_max)
-        x = waveset[mask]
-        x = np.append(np.append(wave_min, x), wave_max)
-        y = spec(x).value
+        wave = np.array([wave_min, *waveset[mask], wave_max])
+        flux = spec(wave).value
 
-        # flux [ph s-1 cm-2] == y [ph s-1 cm-2 AA-1] * x [AA]
+        # flux [ph s-1 cm-2] == flux [ph s-1 cm-2 AA-1] * wave [AA]
         if isinstance(bandpass, SpectralElement):
-            bp = bandpass(x)
             bandpass.model.bounds_error = True
-            counts += [np.trapz(bp * y, x)]
+            counts.append(np.trapz(bandpass(wave).value * flux, wave))
         else:
-            counts += [np.trapz(y, x)]
+            counts.append(np.trapz(flux, wave))
 
     # counts = flux [ph s-1 cm-2]
-    counts = 1E4 * np.array(counts)    # to get from cm-2 to m-2
-    counts *= u.ph * u.s**-1 * u.m**-2
+    counts = (counts * u.ph * u.s**-1 * u.cm**-2).to(u.ph * u.s**-1 * u.m**-2)
     if area is not None:
-        counts *= quantify(area, u.m ** 2)
+        counts *= (area << u.m**2)
 
     return counts
 
 
 def scale_imagehdu(imagehdu, waverange, area=None):
-    # ..todo: implement this
-    # For the moment, all imagehdu must be accompanied by a spectrum in PHOTLAM
-    #
-    # Future functionality will include scaling here of:
-    # ph s-1
-    # ph s-1 m-2
-    # ph s-1 m-2
-    # ph s-1 m-2 um-1
-    # ph s-1 m-2 um-1 arcsec-2
-    # J s-1 m-2 Hz-1
-    # J s-1 m-2 Hz-1 arcsec-2
-    # ABMAG
-    # ABMAG arcsec-2
-    # VEGAMAG
-    # VEGAMAG arcsec-2
+    """
+    ..todo: implement this
 
+    For the moment, all imagehdu must be accompanied by a spectrum in PHOTLAM
+
+    Future functionality will include scaling here of:
+    ph s-1
+    ph s-1 m-2
+    ph s-1 m-2
+    ph s-1 m-2 um-1
+    ph s-1 m-2 um-1 arcsec-2
+    J s-1 m-2 Hz-1
+    J s-1 m-2 Hz-1 arcsec-2
+    ABMAG
+    ABMAG arcsec-2
+    VEGAMAG
+    VEGAMAG arcsec-2
+    """
     if "SPEC_REF" not in imagehdu.header:
         raise ValueError("For this version, an ImageHDU must be associated "
                          "with a spectrum. This will change in the future.")
@@ -150,14 +196,24 @@ def scale_imagehdu(imagehdu, waverange, area=None):
     return imagehdu
 
 
-def make_img_wcs_header(pixel_scale, image_size):
+def make_img_wcs_header(
+    pixel_scale: float,
+    image_size: tuple[int, int],
+) -> fits.Header:
     """
-    Create a WCS header for an image
+    Create a WCS header for an image.
 
+    Parameters
+    ----------
     pixel_scale : float
-        arcsecs
-    image_size : tuple
-        x, y where x, y are integers
+        Pixel scale in arcsecs.
+    image_size : tuple[int, int]
+        Image size (x, y).
+
+    Returns
+    -------
+    TYPE
+        DESCRIPTION.
 
     """
     ra, dec = 0, 0
@@ -167,7 +223,7 @@ def make_img_wcs_header(pixel_scale, image_size):
     imgwcs.wcs.ctype = ["RA---TAN", "DEC--TAN"]
     imgwcs.wcs.cunit = [u.deg, u.deg]
     imgwcs.wcs.crpix = [(x + 1) / 2, (y + 1) / 2]
-    imgwcs.wcs.cdelt = np.array([-pixel_scale / 3600, pixel_scale / 3600])
+    imgwcs.wcs.cdelt = np.array([-pixel_scale, pixel_scale]) / 3600
     imgwcs.wcs.crval = [ra, dec]
     imgwcs.wcs.cunit = [u.deg, u.deg]
 
