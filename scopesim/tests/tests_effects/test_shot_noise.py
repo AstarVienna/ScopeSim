d0671bac4359fc86759164c5d3c83a72fd255b77
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2024-10-29 21:03:38 +0100

    Improve test

--- scopesim/tests/tests_effects/test_shot_noise.py
+++ scopesim/tests/tests_effects/test_shot_noise.py
@@ -14,18 +14,27 @@ class TestInit:
 
 class TestApplyTo:
     def test_applies_dark_current_with_level_of_dit(self):
+        """Test whether ShotNoise is applied correctly."""
         hw = 1
+        # A normal value that works with numpy.random.poisson.
+        value_normal = 1000
+        # A value too large fro numpy.random.poisson.
+        value_too_high = 1e20
         hdr = header_from_list_of_xy([-hw, hw], [-hw, hw], 1, "D")
         dtcr = Detector(hdr)
-        dtcr._hdu.data[0][0] = 100
+        dtcr._hdu.data[0][0] = value_normal
         dtcr._hdu.data[0][1] = -1
-        dtcr._hdu.data[1][0] = 2e33
+        dtcr._hdu.data[1][0] = value_too_high
         dtcr._hdu.data[0][1] = np.nan
 
         sn = ShotNoise()
         dtcr = sn.apply_to(dtcr)
 
-        assert 1 < dtcr._hdu.data[0][0] < 1000
+        # Ensure that the values have changed.
+        assert dtcr._hdu.data[0][0] != value_normal
+        assert dtcr._hdu.data[1][0] != value_too_high
+
+        # Sensibility checks on the values.
+        assert 1 < dtcr._hdu.data[0][0] < value_normal * 2
         assert np.isnan(dtcr._hdu.data[0][1]) or dtcr._hdu.data[0][1] == 0
-        assert dtcr._hdu.data[1][0] == 2e33
         assert np.isnan(dtcr._hdu.data[0][1])
