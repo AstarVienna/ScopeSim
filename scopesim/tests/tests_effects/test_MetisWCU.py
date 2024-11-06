fb8457065734c99ceb2e05f9ee0e02e8bf821658
oczoske <ollicz68@yahoo.com>
2024-11-06 16:28:08 +0100

    update test, add more

--- scopesim/tests/tests_effects/test_MetisWCU.py
+++ scopesim/tests/tests_effects/test_MetisWCU.py
@@ -1,13 +1,29 @@
 """Tests for METIS WCU classes"""
 
 import pytest
+from astropy import units as u
 
 from scopesim.effects.metis_wcu import BlackBodySource
 
 @pytest.fixture(name="bbsource", scope="function")
 def fixture_bbsource():
-    return BlackBodySource()
+    return BlackBodySource(bb_temp=1000*u.K, wcu_temp=300*u.K)
 
 class TestBlackBodySource:
     def test_initialises_correctly(self, bbsource):
         assert isinstance(bbsource, BlackBodySource)
+
+    def test_bbsource_has_temperatures(self, bbsource):
+        assert bbsource.meta['bb_temp'] == 1000 * u.K
+        assert bbsource.meta['wcu_temp'] == 300 * u.K
+
+    def test_bbsource_has_table(self, bbsource):
+        assert bbsource.surface.table
+
+    def test_bbsource_table_has_correct_columns(self, bbsource):
+        assert (bbsource.surface.table.colnames ==
+                ['wavelength', 'transmission', 'emission'])
+
+    def test_bbsource_has_correct_emission_units(self, bbsource):
+        assert (bbsource.surface.table['emission'].unit ==
+                u.ph / (u.s * u.sr * u.um * u.m**2))
