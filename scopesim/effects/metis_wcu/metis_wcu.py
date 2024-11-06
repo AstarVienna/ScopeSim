3c2fc2dcc963ac96d693087c30f160a30ea440b4
oczoske <ollicz68@yahoo.com>
2024-11-06 16:08:48 +0100

    add emission to BlackBodySource

--- scopesim/effects/metis_wcu/metis_wcu.py
+++ scopesim/effects/metis_wcu/metis_wcu.py
@@ -1,8 +1,11 @@
 """Classes for the METIS Warm Calibration Unit"""
 
 import numpy as np
+from astropy.table import Table
+from astropy.modeling.models import BlackBody
+from astropy import units as u
 from ..ter_curves import TERCurve
-from ...utils import get_logger
+from ...utils import get_logger, seq
 
 logger = get_logger(__name__)
 
@@ -20,7 +23,31 @@ class BlackBodySource(TERCurve):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         params = {
-            "z_order": [113, 513]
+            "z_order": [113, 513],
+            "action": "emission",
+            "position": 0,  # position in surface table
         }
         self.meta.update(params)
         self.meta.update(kwargs)
+
+        self.compute_emission()
+
+
+    def compute_emission(self):
+        """Compute the emission at the exit of the integrating sphere"""
+        bb_temp = self.meta["bb_temp"] << u.K
+        wcu_temp = self.meta["wcu_temp"] << u.K
+        tbl = Table()
+        lam = seq(2.2, 15, 0.01) * u.um
+        bb_scale = 1 * u.ph / (u.s * u.m**2 * u.sr * u.um)
+        bb_lam = (BlackBody(bb_temp, scale=bb_scale) +
+                  BlackBody(wcu_temp, scale=bb_scale))
+        flux = bb_lam(lam)
+        tbl.add_column(lam, name="wavelength")
+        tbl.add_column(np.zeros_like(lam).value, name="transmission")
+        tbl.add_column(flux, name="emission")
+        tbl.meta["wavelength_unit"] = tbl["wavelength"].unit
+        tbl.meta["emission_unit"] = tbl["emission"].unit
+
+        self.surface.table = tbl
+        self.surface.meta.update(tbl.meta)
