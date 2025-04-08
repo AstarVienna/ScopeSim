336657e1f4bbf88396fc19b797c7b6bfdb357476
teutoburg <ghost@instruct.at>
2025-04-08 13:27:36 +0200

    Turn prints into logging

--- scopesim/effects/electronic/dmps.py
+++ scopesim/effects/electronic/dmps.py
@@ -72,7 +72,7 @@ class DetectorModePropertiesSetter(Effect):
         self.mode_properties = kwargs["mode_properties"]
 
     def apply_to(self, obj, **kwargs):
-        print("Entering DetectorModePropertiesSetter.apply_to")
+        logger.debug("Entering DetectorModePropertiesSetter.apply_to")
         mode_name = kwargs.get("detector_readout_mode",
                                from_currsys("!OBS.detector_readout_mode",
                                             self.cmds))
@@ -85,7 +85,7 @@ class DetectorModePropertiesSetter(Effect):
         self.cmds["!OBS.detector_readout_mode"] = mode_name
         for key, value in props_dict.items():
             self.cmds[key] = value
-            print(key, value)
+            logger.debug("DMPS setting %s: %s", key, value)
 
         return obj
 
