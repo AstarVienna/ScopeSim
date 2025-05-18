a7c311908a7c13063ab3fa72b4f93713c74a31fd
teutoburg <ghost@instruct.at>
2025-05-18 17:19:55 +0200

    Add simple ifu mode to basic instrument

--- scopesim/tests/test_basic_instrument/test_basic_instrument.py
+++ scopesim/tests/test_basic_instrument/test_basic_instrument.py
@@ -230,7 +230,7 @@ class TestFitsHeader:
         hdr = hdul[0].header
 
         assert hdr["SIM SRC0 object"] == 'star'
-        assert hdr["SIM EFF14 class"] == 'SourceDescriptionFitsKeywords'
+        assert hdr["SIM EFF15 class"] == 'SourceDescriptionFitsKeywords'
         assert hdr["SIM CONFIG OBS filter_name"] == 'J'
         assert hdr["ESO ATM SEEING"] == opt.cmds["!OBS.psf_fwhm"]
 
