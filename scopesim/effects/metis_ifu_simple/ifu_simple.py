fd00b76a733dc182dc147e8e84385be84d021b8b
oczoske <ollicz68@yahoo.com>
2025-05-21 23:14:32 +0200

    LSF works

--- scopesim/effects/metis_ifu_simple/ifu_simple.py
+++ scopesim/effects/metis_ifu_simple/ifu_simple.py
@@ -3,11 +3,15 @@
 
 from typing import ClassVar
 
+import numpy as np
+from astropy.convolution import convolve, Box1DKernel, Gaussian1DKernel
+
 from ..effects import Effect
 from ...optics.fov import FieldOfView
 from ...utils import from_currsys, get_logger
 
 logger = get_logger(__name__)
+
 
 class LineSpreadFunction(Effect):
     """
@@ -26,13 +30,52 @@ class LineSpreadFunction(Effect):
         self.meta.update(kwargs)
         self.meta = from_currsys(self.meta, self.cmds)
 
-        self.lsfwidth = None #self.get_width()
-        self.kernel = None   #self.get_kernel()
+        if "lsfwidth" not in self.meta:
+            self.lsfwidth = self.get_lsf_width()
+        else:
+            self.lsfwidth = self.meta["lsfwidth"]
+        self.kernel = self.get_kernel()
 
-    def apply_to(self, obj, **kwargs):
+    def apply_to(self, fov, **kwargs):
         """Apply the LSF"""
-        if not isinstance(obj, FieldOfView):
-            return obj
+        if not isinstance(fov, FieldOfView):
+            return fov
 
-        lamc = from_currsys("!OBS.wavelen", self.cmds)
-        print(">>>>>>>>> Central wavelength:", lamc)
+        if fov.hdu is None or fov.hdu.header["NAXIS"] != 3:
+            logger.error("Cannot apply LSF convolution")
+            return fov
+
+
+        print("Happily convolving with LSF")
+        fov.hdu.data = convolve(fov.hdu.data, self.kernel)
+        fov.cube = fov.hdu
+        return fov
+
+    def get_lsf_width(self):
+        """Determine width of the LSF kernel at central wavelength"""
+
+        slope = self.meta['fit_slope']
+        intercept = self.meta['fit_intercept']
+        lamc = self.meta["wavelen"]
+        dlam_per_pix = slope * lamc + intercept
+
+        slice_width = self.meta['slice_width']
+        pixel_scale = self.meta['pixel_scale']
+
+        dlam_per_slice = dlam_per_pix * slice_width / pixel_scale
+
+        spec_binwidth = self.meta["spec_binwidth"]
+
+        return dlam_per_slice / spec_binwidth
+
+
+    def get_kernel(self):
+        """Build LSF kernel: box kernel smoothed with narrow Gauss"""
+
+        box = Box1DKernel(width=self.lsfwidth)
+        gauss = Gaussian1DKernel(1)
+        if box.shape > gauss.shape:
+            kernel = convolve(box.array, gauss)[np.newaxis, np.newaxis, :]
+        else:
+            kernel = convolve(gauss.array, box)[np.newaxis, np.newaxis, :]
+        return kernel
