06af6cfbd84de2c94aa3770e782d34342372563d
teutoburg <ghost@instruct.at>
2025-07-14 13:59:11 +0200

    Remove empty `spectrograph.py`

--- scopesim/optics/__init__.py
+++ scopesim/optics/__init__.py
@@ -8,8 +8,6 @@ from .optical_element import OpticalElement
 from .optical_train import OpticalTrain
 from .optics_manager import OpticsManager
 
-from . import spectrograph
-
 from .surface import SpectralSurface
 from . import surface_utils
 from . import radiometry_utils
