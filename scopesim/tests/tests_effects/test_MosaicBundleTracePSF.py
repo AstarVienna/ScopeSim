237811cf673542c985c08d3eab4182b67294a4a7
teutoburg <ghost@instruct.at>
2025-07-15 14:23:07 +0200

    Add PSFs from @astronomyk's MOS branch

---
+++ scopesim/tests/tests_effects/test_MosaicBundleTracePSF.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""From psfs/analytical."""
+
+from matplotlib import pyplot as plt
+
+from scopesim.effects.psfs.analytical import MosaicBundleTracePSF, AnalyticalPSF
+
+PLOTS = False
+
+
+class TestInit:
+    def test_initialised_with_nothing(self):
+        assert isinstance(MosaicBundleTracePSF(), AnalyticalPSF)
+
+    def test_initalises_with_radial_profile(self):
+        mbt_psf = MosaicBundleTracePSF(r=[0, 3.5, 4.2],
+                                       z=[1,   1,   0])
+        assert isinstance(mbt_psf, MosaicBundleTracePSF)
+
+
+class TestGetKernel:
+    def test_returned_kernel_is_zipped_version_of_single_kernel(self):
+        mbt_psf = MosaicBundleTracePSF(n_traces_per_bundle=2,
+                                       trace_spacing=2.1,
+                                       trace_flux_weights=[0.5, 2, 1])
+        kernel = mbt_psf.get_kernel(None)
+
+        if PLOTS:
+            plt.imshow(kernel)
+            plt.show()
+
+        ws = mbt_psf.meta["trace_flux_weights"]
+        assert kernel.sum() == sum(ws) * mbt_psf.single_kernel.sum()
+        # TODO: Not sure what this is supposed to test, but it fails
+        #       Perhaps the second one should be the single_kernel??
+        # assert kernel.shape[0] > ws * kernel.shape[0]
