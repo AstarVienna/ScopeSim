f37016765dec4af64b7a5c571c2f19da0823e86d
teutoburg <ghost@instruct.at>
2026-08-30 23:05:14 +0200

    Remove `apply_throughput_to_cube`
    This was only used in two places, and one of them has now been refactored
    to use something else, so no point in keeping the separate function,
    which in the end was just a one-liner anyway.

--- scopesim/effects/ter_curves_utils.py
+++ scopesim/effects/ter_curves_utils.py
@@ -359,36 +359,6 @@ def scale_spectrum(spectrum, filter_name, amplitude):
     return spectrum
 
 
-def apply_throughput_to_cube(
-    cube: fits.ImageHDU,
-    thru: SpectralElement | SourceSpectrum,
-    wave_cube: u.Quantity,
-) -> fits.ImageHDU:
-    """
-    Apply throughput curve to a spectroscopic cube.
-
-    Parameters
-    ----------
-    cube : fits.ImageHDU
-        Three-dimensional image, dimension 0 (in python convention) is the
-        spectral dimension.
-    thru : SpectralElement | SourceSpectrum
-        Throughput curve, spectrum or filter.
-    wave_cube : u.Quantity["length"]
-        Wavelength axis of the cube.
-
-    Returns
-    -------
-    cube : fits.ImageHDU
-        Header unchanged, data multiplied with wavelength-dependent throughput.
-
-    """
-    # Note: wave_cube used to be converted to AA, but synphot understands um
-    #       or whatever just as well...
-    cube.data *= thru(wave_cube).value[:, None, None]
-    return cube
-
-
 def add_edge_zeros(tbl, wave_colname):
     if isinstance(tbl, Table):
         vals = np.zeros(len(tbl.colnames))
